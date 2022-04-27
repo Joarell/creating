c8b7528a36dd9c953d965a506bd358f9012bac5d
Joarell <josephjoarell@gmail.com>
2022-04-27 01:18:07 -0300

    Some progress were done. Now I've got the work code. The next step is making the whole dictionary.

--- test.py
+++ test.py
@@ -24,26 +24,21 @@ temp = str(lines[0])
 
 works.close()
 works = open("works.txt")
-code = works.read().split(',')
+code = works.read().split('\n')
 yank = code[1].split('\n')
 dim = yank[0]
 
 print(f'The work code is: {code[0]}')
 print(f'The dimensions are: {dim}')
 
-work_l = {code[0]: dim}
-print(work_l)
+yank = int()
+yank = code[0].split(',')
+yank = yank[0]
+print(code)
 
-works.close()
-works = open("works.txt")
-code = works.read().split(',')
-
-i = 1
-work_l = dict()
-while code[i] is True:
-    yank = code[i].split('\n')
-    dim = yank[i - 1]
-    work_l[code[i - 1]] = yank
+i = 0
+while yank != "":
+    print(f'Work code: {yank}')
     i += 1
-
-print(work_l)
+    yank = code[i].split(',')
+    yank = yank[0]
