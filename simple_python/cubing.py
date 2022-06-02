b3bbe0a672b9143e0cc70ae9ae203577747294d8
Joarell <josephjoarell@gmail.com>
2022-06-01 21:22:18 -0300

    cubing module added.

---
+++ simple_python/cubing.py
@@ -0,0 +1,5 @@
+def cubing_calc(sizes):
+    """ This function is to do calculation
+    of the cube dimensions of work or crate."""
+    cubing_total = sizes[0] * sizes[1] * sizes[2]/6000
+    return cubing_total
