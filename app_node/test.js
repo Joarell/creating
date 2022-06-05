9b07f2d781e843caebd7e0cd90334fec0c5f704e
Joarell <josephjoarell@gmail.com>
2022-06-04 21:51:22 -0300

    Node versioon of the app.

---
+++ app_node/test.js
@@ -0,0 +1,12 @@
+const cub = require('./app.js');
+
+const work = [100, 5, 100];
+console.log(cub.cubing(work));
+
+// function cub(sizes)
+// {
+// 	return sizes[0] * sizes[1] * sizes[2]/6000
+// }
+//
+// let a_Work = [100, 5, 100];
+// console.log(Math.floor((cub(a_Work))*1000)/1000)
