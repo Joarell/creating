a96ca65e6792d26d3e0a00b580b1cb031a064eee
Joarell <josephjoarell@gmail.com>
2022-06-11 16:40:10 -0300

    cubing module updated - comments out and working.

--- app_node/cubing.js
+++ app_node/cubing.js
@@ -1,10 +1,7 @@
-function cubing(dimensions) {
+function cubing(dimensions)
+{
 	const cubed = dimensions[0] * dimensions[1] * dimensions[2]/6000;
 	return Math.floor(cubed * 1000)/1000;
 }
 
 module.exports = { cubing };
-
-// console.log("The cubed value is: " + result);
-// const round = (x) => Math.floor(x);
-// console.log(round(result));
