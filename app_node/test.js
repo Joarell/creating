4e846201ec1ab5522f9b30b4a8bcafed4c707cf6
Joarell <josephjoarell@gmail.com>
2022-06-11 16:41:20 -0300

    Test file updated for new module big_work.

--- app_node/test.js
+++ app_node/test.js
@@ -1,12 +1,14 @@
-const cub = require('./app.js');
+const cub = require('./cubing.js');
+const big = require('./big_work.js');
+let test = 0;
 
+
+// Test: cube funciton
 const work = [100, 5, 100];
-console.log(cub.cubing(work));
 
-// function cub(sizes)
-// {
-// 	return sizes[0] * sizes[1] * sizes[2]/6000
-// }
-//
-// let a_Work = [100, 5, 100];
-// console.log(Math.floor((cub(a_Work))*1000)/1000)
+// Test: big work function
+const w_list = [[150, 5, 100], [200, 5, 180]];
+
+test = big.big_work(w_list);
+console.log(cub.cubing(work));
+console.log(test);
