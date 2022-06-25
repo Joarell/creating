ef0d63cfecce00f337a66dc69bdd82f1e3efc9bf
Joarell <josephjoarell@gmail.com>
2022-06-24 23:13:01 -0300

    app updated and running.

--- app_node/test.js
+++ app_node/test.js
@@ -3,10 +3,10 @@ const big = require('./big_work.js');
 let test = 0;
 
 
-// Test: cube funciton
+// Test: cube module.
 const work = [100, 5, 100];
 
-// Test: big work function
+// Test: big work module.
 const w_list = [[150, 5, 100], [200, 5, 180]];
 
 test = big.big_work(w_list);
