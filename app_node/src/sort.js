76af75a96627cba60e93cf0d8c9d7ea4c037be41
Joarell <josephjoarell@gmail.com>
2022-07-04 15:10:39 -0300

    sort module is 50% done.

--- app_node/src/sort.js
+++ app_node/src/sort.js
@@ -1,4 +1,4 @@
-let bigger = require ("./big_work.js");
+let cube_size = require ("./cubing.js");
 const work_list =
 {
 	1298: "200, 05, 100",
@@ -8,6 +8,7 @@ const work_list =
 	777: "50, 03, 50"
 }
 
+// This function is needed to get the string values from the Object and parse them to int.
 function split_int(dimensions)
 {
 	dimensions = dimensions.split(",");
@@ -22,13 +23,14 @@ function split_int(dimensions)
 	return work_dimensions;
 }
 
-function get_dimensions(w_list)
+// This function is resonsible to get the dimensions from the list.
+function get_dimensions(w_list, work)
 {
 	let i = 0;
 	let hold = 0; 
 	let dimensions = [];
 
-	while (i <= Object.keys(w_list).length)
+	while (i < work)
 	{
 		hold = (Object.values(w_list)[i]);
 		dimensions.push(split_int(hold));
@@ -37,16 +39,45 @@ function get_dimensions(w_list)
 	return dimensions;
 }
 
-
-// function sort(work_list)
+// function get_code(w_list)
 // {
+// 	let code = 0;
+// 	let works = [];
 //
+// 	while (code < Object.keys(w_list).length)
+// 	{
+// 		works.push(Object.keys(w_list)[code]);
+// 		code++;
+// 	}
+// 	return works;
 // }
 
+function sort(w_list)
+{
+	let work = 0;
+	let i = 0;
+	let j = 1;
+	let key = 0;
+
+	for (i in w_list)
+	{
+		key = get_dimensions(Object.values(w_list)[j]);
+		work = get_dimensions(Object.values(w_list)[i]);
+		key = cube_size.cubing(key);
+		work = cube_size.cubing(work);
+
+		i = j - 1;
+		whiel (i >= 0 && work > key)
+		{
+			w_list
+		}
+	}
+}
+
 // let first = Object.values(work_list)[0];
-let test = Object.values(work_list)[0];
+// let test = Object.values(work_list)[0];
 // console.log(get_dimensions(work_list));
 // test = test.split(",")
-let first = split_int(work_list);
-
-console.log(first);
+// let first = get_code(work_list);
+console.log(get_dimensions(Object.values(work_list)[0], 1));
+console.log(cube_size(get_dimensions(Object.values(work_list)[0])));
