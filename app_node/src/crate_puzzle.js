47cd27729f564f89b188922dfd0579c0546c0038
Joarell <josephjoarell@gmail.com>
2022-07-08 13:55:50 -0300

    Function reformulated in order to engage correctly.

--- app_node/src/crate_puzzle.js
+++ app_node/src/crate_puzzle.js
@@ -1,20 +1,7 @@
 //This function returns if the work feats or not in to the crate sizes.
 function puzzle(c, w)
 {
-	const layer = [];
-	let i = 0;
-
-	for (i in c && w)
-	{
-		if (i == 1)
-		{
-			layer.push(c[i]);
-		}
-		else
-		{
-			layer.push(c[i] - w[i]);
-		}
-	}
+	const layer = c - w;
 	return layer;
 }
 
