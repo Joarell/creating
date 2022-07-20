5f9ed09c9fbca1526ad64bba13247b72c820d8f1
Joarell <josephjoarell@gmail.com>
2022-07-20 18:13:23 -0300

    Just a better name for the function.

---
+++ app_node/src/layer_puzzle.js
@@ -0,0 +1,8 @@
+//This function returns if the work feats or not in to the crate sizes.
+function layer_puzzle(c, w)
+{
+	const layer = c - w;
+	return layer;
+}
+
+module.exports = { layer_puzzle };
