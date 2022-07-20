097c97fda6b0004d3900bae5bf86c647698182ff
Joarell <josephjoarell@gmail.com>
2022-07-20 18:15:04 -0300

    Sort module had some improvements. 95% done.

--- app_node/src/sort.js
+++ app_node/src/sort.js
@@ -60,7 +60,7 @@ function zipper(codes, cubes, index)
 }
 
 
-//This function acts sorting the smallest workt to the biggest one.
+//This function acts sorting the smallest work to the biggest one.
 function i_sort(works)
 {
 	let i = 0;
