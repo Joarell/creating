6f2ca7f38e47af3a56f6eaaf9d26f2465330eecb
Joarell <josephjoarell@gmail.com>
2022-07-20 18:17:26 -0300

    Labor module 10% done.

--- app_node/src/labor.js
+++ app_node/src/labor.js
@@ -1,22 +1,21 @@
-let def_crate = require("./big_work.js");
-let defautl_crate = def_crate(work_list);
+//Aqui precismos realizar algumas tratativas para a conclusão do algorítmo:
+//	1 - Obter a lista de obras com os códigos e dimensões;
+//	2 - Tratar a lista retornarndo para uma váriável os valores cubados do menor parao maior com a função "sort";
+//	3 - Desenvoler a algoritmo de backtrack para construção e acondicionamento das obras dentro da caixa.
+//	4 - 
 
-
-function invert(work)
+const work_list =
 {
-	let long = work[2];
-	let height = work[0];
-	work[0] = long;
-	work[2] = height;
-	return work;
+	1298: "200, 05, 100",
+	123: "100, 05, 100",
+	5908: "150, 05, 90",
+	8899: "120, 03, 100",
+	777: "50, 03, 50",
 }
 
+let sort_list = require("./sort.js");
+sort_list = sort_list.i_sort(work_list);
+const last = sort_list.length;
+let biggest = sort_list[last - 1][1];
 
-function packing(work_list)
-{
-	crate = defautl_crate;
-
-
-
-
-}
+console.log(biggest);
