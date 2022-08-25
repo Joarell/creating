ecb1915ff51a58382766c369491259e79e38fef9
Joarell <josephjoarell@gmail.com>
2022-08-25 02:22:58 -0300

    Just a little change.

--- app_node/src/cubing.js
+++ app_node/src/cubing.js
@@ -1,7 +1,7 @@
 //This function returns the cubed value to the work.
 function cubing(dimensions)
 {
-	const cubed = dimensions[0] * dimensions[1] * dimensions[2]/6000;
+	const cubed = dimensions[0] * dimensions[1] * dimensions[2]/1000000;
 	return Math.floor(cubed * 1000)/1000;
 }
 
