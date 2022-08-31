9c1206325fbaefb05a5c1e68a0a52be79fbb154b
joarell <josephjoarell@gmail.com>
2022-08-31 02:17:54 -0300

    layer_puzzle module is almost done. Now I should figure it out how to define the sizes subtraction properly.

--- app_node/src/layer_puzzle.js
+++ app_node/src/layer_puzzle.js
@@ -18,8 +18,8 @@ const work_list = {
 }
 
 
-let sort_list = require("./sort.js");
 let next_work = require("./next_work.js");
+let sort = require("./sort.js");
 
 
 //This function returns the optimize arrays to each layer.
@@ -37,62 +37,98 @@ function layerDone(work_done, layer)
 }
 
 
+function featingCrate(crate_sizes, piece_sizes, crate)
+{
+	let result = [];
+	let x;
+	let y;
+
+	if (crate_sizes[0] >= piece_sizes[0] && !crate[1])
+	{
+		x = crate_sizes[0] - piece_sizes[0];
+		y = crate_sizes[1];
+	}
+	else
+	{
+		x = crate_sizes[0];
+		y = crate_sizes[1] - piece_sizes[1];
+	}
+	result.push(x);
+	result.push(y);
+	return (result);
+}
+
+
 //This function is responsible to feat the works in to the crate layers.
 function labor(crate_dim, works, layer, crate)
 { 
-	let filled;
+	let piece = [];
 	let len;
+	let new_size;
 
-	len = nextWork(crate_dim, works, works.length);
+	len = next_work.nextWork(crate_dim, works, works.length);
 	if (len > 0)
-		filled = works[len][1];
-	if (len == 0)
 	{
-		crate.unshift(works.splice(0, 1));
-		return crate = layerDone(crate, layer);
+		piece.push(works[len][1]);
+		piece.push(works[len][3]);
 	}
-	crate.unshift(works.splice(len, 1));
-	return labor(crate_dim - filled, works, layer, crate);
+	else if (len == 0)
+	{
+		crate.push(works.splice(0, 1));
+		crate = layerDone(crate, layer);
+		return works;
+	}
+	new_size = featingCrate(crate_dim, piece, crate);
+	crate_dim.splice(0, 2);
+	crate_dim.push(new_size[0]);
+	crate_dim.push(new_size[1]);
+	crate.push(works.splice(len, 1));
+	return labor(crate_dim, works, layer, crate);
 }
 
 
 //This function returns the all the crates needed accordingly the work list.
 function solveList(the_list)
 {
-	sort_list = sort_list.newArraySorted(the_list);
 	let crate_finished = [];
 	let crates = [];
-	let standard_layer = 0;
-	let new_crate = 0;
-	let layer = 2;
+	let standard_layer = [];
+	let new_crate;
+	let layer;
+	let new_list;
 	let tmp = [];
 
-	while(sort_list.length > 0)
+	new_list = sort.getDimensions(the_list);
+	new_list = sort.quickSort(next_work.cubVersionList(new_list), 4);
+	layer = 1;
+	while(new_list.length > 0)
 	{
-		if (layer == 2)
+		if (layer === 1)
 		{
-			standard_layer = sort_list.pop();
-			crate_finished.push(standard_layer);
-			crate_finished.unshift(1);
-			standard_layer = standard_layer[1];
+			standard_layer = next_work.standardLayer(new_list);
+			crate_finished.push(layer);
 		}
-		else if (layer == 4)
+		else if (layer === 4)
 		{
 			new_crate++;
-			crates.unshift(crate_finished);
-			crates.unshift(new_crate);
+			crates.push(new_crate);
+			crates.push(crate_finished);
 			crate_finished.splice(0, crate_finished.length);
 			layer = 1;
 		}
-		crate_finished.push(labor(standard_layer, sort_list, layer, tmp));
-		tmp.splice(0, tmp.length);
+		tmp.push(standard_layer[0]);
+		tmp.push(standard_layer[1]);
+		crate_finished.push(labor(tmp, new_list, layer, crate_finished));
 		layer++;
+		tmp.splice(0, 2);
 	}
-	if (crates == 0)
+	if (crates.length === 0)
 		return crate_finished;
 	return crates;
 }
 
 
 module.exports = { solveList };
-console.log(solveList(work_list));
+let arrange = solveList(work_list);
+console.log(arrange);
+
