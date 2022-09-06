e1a3568b31312fa63b27c16df6c62c7c43b986d8
joarell <josephjoarell@gmail.com>
2022-09-06 01:10:49 -0300

    Some improvements in the layer_puzzle to reach largest works and furniture. Now I'll work on the final sizes of the work adding the layer space and some marks for turned works to fit in side the crate

--- app_node/src/layer_puzzle.js
+++ app_node/src/layer_puzzle.js
@@ -1,5 +1,5 @@
-let next_work = require("./next_work.js");
-let sort = require("./sort.js");
+const next_work = require("./next_work.js");
+const sort = require("./sort.js");
 
 
 //This function returns the the swap of the work dimensions.
@@ -21,16 +21,16 @@ function workSwapNinety(work)
 
 //This function returns the new empty size into the crate after subtract the
 //dimensions between crate and piece sizes.
-function featingCrate(crate_sizes, piece_sizes)
+function fitingCrate(crate_sizes, piece_sizes)
 {
-	let result = [];
+	let result;
 	let x;
 	let y;
 	let arrange = 1;
 
 	if((crate_sizes[0] === piece_sizes[0] && crate_sizes[1] === piece_sizes[1]) ||
 	(crate_sizes[1] === piece_sizes[0] && crate_sizes[0] === piece_sizes[1]))
-		return	result = [0, 0];
+		return	(result = [0, 0]);
 	while (arrange--)
 	{
 		if ((crate_sizes[0] >= piece_sizes[0] && crate_sizes[1] > piece_sizes[1]) ||
@@ -50,9 +50,7 @@ function featingCrate(crate_sizes, piece_sizes)
 			arrange++;
 		}
 	}
-	result.push(x);
-	result.push(y);
-	return (result);
+	return (result = [x, y]);
 }
 
 
@@ -70,16 +68,30 @@ function labor(crate_dim, works, layer, crate)
 	}
 	piece.push(works[len][1]);
 	piece.push(works[len][3]);
-	crate_dim = Array.from(featingCrate(crate_dim, piece));
+	crate_dim = Array.from(fitingCrate(crate_dim, piece));
 	crate.push(works.splice(len, 1));
 	return labor(crate_dim, works, layer, crate);
 }
 
 
-//This function provides the crate with all possble works in.
+//This function eliminates the extra array provided by labor and
+//noCanvasOut functions.
+function arrayCleaner(list)
+{
+	len = list.length;
+	while (len--)
+	{
+		list = list.concat(list[len]);
+		list.splice(len, 1);
+	}
+	return (list);
+}
+
+
+//This function provides the crate with all possible works inside.
 function crateArrange(standard_size, list, layer)
 {
-	let tmp;
+	let tmp = [];
 	let crate_defined = [];
 
 	while (layer++ <= 3 && list.length > 0)
@@ -88,7 +100,7 @@ function crateArrange(standard_size, list, layer)
 		tmp = Array.from(standard_size);
 		labor(tmp, list, layer, crate_defined);
 	}
-	return (crate_defined);
+	return (arrayCleaner(crate_defined));
 }
 
 
@@ -98,18 +110,55 @@ function solveListProcedure(the_list)
 	let crates_done = [];
 	let standard_layer = [];
 	let new_list;
+	let largests = []
 
 	new_list = sort.getDimensions(the_list);
 	new_list = sort.quickSort(next_work.cubVersionList(new_list), 4);
+	next_work.noCanvasOut(new_list, new_list.length, largests);
 	layer = 0;
 	while (new_list.length > 0)
 	{
-		standard_layer = next_work.standardLayer(new_list);
-		crates_done.push(standard_layer);
-		crates_done = crates_done.concat(crateArrange(standard_layer, new_list, layer));
-		layer = 0;
+		if (largests != 0)
+		{
+			largests = arrayCleaner(largests);
+			standard_layer = next_work.standardLayer(largests);
+			standard_layer = next_work.largestWorks(largests, standard_layer);
+			crates_done = crates_done.concat(crateArrange(standard_layer, largests, layer));
+			crates_done.push(standard_layer);
+		}
+		else
+		{
+			standard_layer = next_work.standardLayer(new_list);
+			crates_done = crates_done.concat(crateArrange(standard_layer, new_list, layer));
+			crates_done.push(standard_layer);
+			layer = 0;
+		}
 	}
 	return (crates_done);
 }
 
 module.exports = { solveListProcedure };
+const work_list = {
+
+	1298: "200, 05, 100", //first
+	123: "100, 05, 100",
+	5908: "150, 05, 90",
+	8899: "120, 03, 100",
+	777: "50, 03, 50",
+	8980: "30, 03, 30",
+	71234: "30, 03, 30",
+	1111: "30, 03, 30",
+	2313: "30, 03, 30",
+	1112: "60, 05, 90",
+	1897: "180, 05, 100", //second
+	9897: "75, 05, 80",
+	09884: "100, 05, 120",
+	8745: "130, 05, 100",
+	8877: "160, 05, 160", //third
+	7777: "100, 05, 160",
+	8888: "45, 45, 45",
+	90890: "50, 50, 50",
+	12345: "100, 10, 190"
+}
+
+console.log(solveListProcedure(work_list));
