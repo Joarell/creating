73020f153972fc2c5a60cff1c54db616d77941ea
Joarell <josephjoarell@gmail.com>
2022-10-05 23:33:30 -0300

    Almost done. 99%. Now all the functions are set. Just a few adjustments on the: spliSectionCrateFour and child functions from there.

--- app_node/src/labor_same_sizes.js
+++ app_node/src/labor_same_sizes.js
@@ -1,3 +1,67 @@
+//
+function largeCrate(list, sizes, dimensions) {
+	let new_crate;
+	let len;
+	let x;
+	let z;
+	let y;
+
+	height = 145;
+	len = list.length;
+	x = dimensions[0];
+	z = dimensions[1];
+	y = list[len - 2][3];
+	while (x >= 0 || len > 0) {
+		if (list[len - 2][3] <= y && x >= list[len - 2][1]) {
+			x -= list[len - 2][1];
+			list.splice(len - 2, 2);
+			sizes.splice(sizes.length - 1, 1);
+		}
+		len -= 2;
+		if (x - list[len - 2][1] < 0)
+			break;
+	}
+	if (x < dimensions[0]) {
+		x = dimensions[0];
+		y += dimensions[2];
+		new_crate = [x, z, y];
+		dimensions = new_crate;
+	}
+	return (dimensions);
+}
+
+
+//This function call the largerCrate funtion to define a new crate to arrange
+// more works in side.
+function doubleCheck(list, sizes, dimensions) {
+	let x;
+	let z;
+	let y;
+	let package;
+	let height;
+
+	i = 0;
+	x = false;
+	z = false;
+	y = false;
+	package = 5;
+	height = 145;
+	if (list[i][1] >= dimensions[0]) {
+		x = true;
+	}
+	if (list[i][2] >= dimensions[1] * package) {
+		z = true;
+	}
+	if (list[i][3] + dimensions[2] <= height) {
+		y = true;
+	}
+	if (!x && !z && y)
+		return (largeCrate(list, sizes, dimensions));
+	else
+		return (dimensions);
+}
+
+
 //This function returns the sizes of the crate dealing with all works with the
 //sizes if the dimensions length if equal to four.
 function splitSectionCrateFour(list, dimensions) {
@@ -5,46 +69,43 @@ function splitSectionCrateFour(list, dimensions) {
 	let y;
 	let z;
 	let definition;
-	let major;
+	let tmp;
 	let i;
 	let package;
 
+	i = 0;
 	package = 5;
-	x = dimensions[0] * package;
-	i = 0;
 	z = list[i][1];
-	major = list[i + 1][1];
+	x = dimensions[0] * package;
+	tmp = list[i][1];
 	while (i <= dimensions.length) {
-		if (list[i][1] < list[i + 1][1]) {
-			z = major;
-			major = list[i + 1][1];
+		if (list[i][1] < list[i + 2][1]) {
+			z = tmp;
+			tmp = list[i + 2][1];
 		}
-		i++;
+		i += 2;
 	}
+	if (tmp > z)
+		z = tmp;
 	i = 0;
-	while (i <= dimensions.length) {
-		if (z != list[i][1] || major != list[i][1])
+	while (i < list.length) {
+		if (z === list[i][1]) {
 			y = list[i][3];
-		if (y < list[i][3])
-			y = list[i][3];
-		i++;
+			list.splice(i, 2);
+			if (i > dimensions.length)
+				dimensions.splice(dimensions.length - 1, 1);
+			else
+				dimensions.splice(i, 1);
+		}
+		i += 2;
 	}
-	if (major > z) {
-		i = list.filter(value => list[1] === major);
-		y += i[3];
-	}
-	else {
-		i = list.filter(value => list[1] === z);
-		y += i[3];
-	}
-	z += major;
 	if (z > x) {
 		i = x;
 		x = z;
 		z = i;
 	}
-	dimensions.slice(0, 4);
-	return definition = [x, z, y];
+	definition = [x, z, y];
+	return (doubleCheck(list, dimensions, definition));
 }
 
 
@@ -112,7 +173,8 @@ function manager(list, dimensions) {
 		return (splitSectionCrateOne(list, dimensions));
 	else if (dimensions.length <= 3)
 		return (splitSectionTwo(list, dimensions));
-	return (splitSectionCrateFour(list, dimensions));
+	else
+		return (splitSectionCrateFour(list, dimensions));
 }
 
 module.exports = { manager };
