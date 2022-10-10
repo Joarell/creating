52ef91b6be541ff49e180172f62d72b89855880b
Joarell <josephjoarell@gmail.com>
2022-10-09 21:07:40 -0300

    These modules were integrated to extras.mant.js. Therefore, they are not required.

--- app_node/src/cubing.system.js
+++
@@ -1,7 +0,0 @@
-//This function returns the cubed value to the work.
-function cubing(dimensions) {
-	const cubed = dimensions[1] * dimensions[2] * dimensions[3] / 1000000;
-	return Math.floor(cubed * 1000) / 1000;
-}
-
-module.exports = { cubing };
