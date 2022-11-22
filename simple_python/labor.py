ea64af2f1374a9ecef656194b2ce277e4768205d
Joarell <josephjoarell@gmail.com>
2022-11-22 00:18:45 -0300

    Big updates to the brand new breach.Done!

--- simple_python/labor.py
+++
@@ -1,8 +0,0 @@
-# This function provides the calculation of the dimensions of the crate works.
-def manual_labor(c, w):
-    crated = []
-    zipp = zip(c, w)
-    for c, w in zipp:
-        crated.append(c - w)
-
-    return crated
