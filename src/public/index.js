0bf5a1b72835d82a5f535762307bc768aa2a12a9
Joarell <josephjoarell@gmail.com>
2022-11-23 20:43:14 -0300

    Some improvements about the front-end and studies. Slow progress, but consistent ones.

--- src/public/index.js
+++ src/public/index.js
@@ -1,12 +1,26 @@
-let add = 0;
+//TODO - Create some protection against blank inputs before pussh any button.
+//	Fields:
+//		Estimate;
+//		Work code;
+//		Length;
+//		Depth;
+//		Height;
+
 let list = {};
+let add = 0;
+let estimate = {};
 
 
-function hiThere() {
-	let message;
+//This function is the main function of the webapp. It solves the art work list
+//to possible crates.
+function crate () {
+	let e_code;
 
-	message = "Hi there! I' working every press time!";
-	document.write(message);
+	e_code = document.getElementById("input_estimate").value;
+	console.log(e_code);
+	estimate["reference"] = e_code;
+	estimate["list"] = list;
+	//In this point the function should call the modules to solve the list.
 }
 
 
@@ -15,9 +29,9 @@ function addWorks () {
 	let message;
 	let counter;
 	
-	add += 1;
+	add++;
 	message = "Counting: ";
-	counter = document.getElementById("count")
+	counter = document.getElementById("count");
 	counter.innerText = message + add;
 	insertWorks();
 	return (counter);
@@ -31,13 +45,15 @@ function removeWorks () {
 	
 	add -= 1;
 	message = "Counting: ";
-	counter = document.getElementById("count")
+	counter = document.getElementById("count");
 	counter.innerText = message + add;
-	//This function needs to ask the artwork code in order to remove it the list.
+	//TODO - This function needs to ask the artwork code in order to remove it
+	//from the list.
 	return (counter);
 }
 
 
+//This function is responsible to add the size works to the list.
 function insertWorks () {
 	let x;
 	let z;
@@ -49,13 +65,19 @@ function insertWorks () {
 	x = document.getElementById("input_len").value;
 	z = document.getElementById("input_dep").value;
 	y = document.getElementById("input_hig").value;
+	//TODO - check if all inputs are valid;
 	work = { code, x, z, y };
-	console.log(work);
+	list["work"] = work;
+	cleanFields();
+	return (list);
+}
 
+
+//This function cleans all fields and puts the cursor in the code input box.
+function cleanFields () {
 	document.getElementById("input_code").value = "";
 	document.getElementById("input_code").select();
 	document.getElementById("input_len").value = "";
 	document.getElementById("input_dep").value = "";
 	document.getElementById("input_hig").value = "";
-	return (work);
 }
