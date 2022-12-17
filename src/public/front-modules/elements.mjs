13a7e058a3d955c2091bfefd40d05d4ebb073c5a
Joarell <josephjoarell@gmail.com>
2022-12-17 00:12:06 -0300

    chore (update): accomplishment done

--- src/public/front-modules/elements.mjs
+++ src/public/front-modules/elements.mjs
@@ -1,6 +1,10 @@
+// ╭───────────────────────────────────────────────────────────────────╮
+// │ Calls to each change on the localStorage to update the list pane. │
+// ╰───────────────────────────────────────────────────────────────────╯
 globalThis.onstorage = () => {
-	if (!sessionStorage.getItem("codes"))
-		sessionStorage.setItem( "codes", []);
+	elementTable();
+}
+globalThis.onload = () => {
 	elementTable();
 }
 
@@ -18,9 +22,10 @@ export function elementTable () {
 		metric = "in";
 	else
 		metric = "cm";
-	i = 1;
-	for (i in localStorage.key(i)) {
-		if (localStorage.key(i) !== "metrica" && localStorage.key(i)){
+	i = 0;
+	createHeader();
+	while (localStorage.key(i)) {
+		if(localStorage.key(i) !== "metrica"){
 			work = JSON.parse(localStorage.getItem(localStorage.key(i)));
 			work = Object.values(work);
 			plot.innerHTML += work.map((item, index) => {
@@ -33,5 +38,28 @@ export function elementTable () {
 				return(`<th>${item}</th>`);
 			}, 0).join("");
 		}
+		i++;
 	}
+}
+
+// ╭──────────────────────────────────────────────────────────────────────────╮
+// │ // This is the header creator when the page or localStorage are updated. │
+// ╰──────────────────────────────────────────────────────────────────────────╯
+function createHeader(){
+	const element = document.getElementById("sizes");
+	const head = document.createElement("tr");
+	
+	if(element.parentNode){
+		while(element.firstChild)
+			element.removeChild(element.firstChild)
+	}
+	head.innerHTML =`
+		<tr>
+			<th>CODE</th>
+			<th>LENGTH</th>
+			<th>DEPTH</th>
+			<th>HEIGHT</th>
+		</tr>
+	`
+	return(element.appendChild(head));
 }
