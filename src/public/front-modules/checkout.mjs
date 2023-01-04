c20b2ef839182fb1f77a161ef59eb0aad12901ee
Joarell <josephjoarell@gmail.com>
2023-01-04 15:39:25 -0300

    build (core): new module to handle on load page

---
+++ src/public/front-modules/checkout.mjs
@@ -0,0 +1,60 @@
+import * as mod from './functions.front.end.mjs'
+
+
+// ╭───────────────────────────────────────────────────────────────────╮
+// │ Calls to each change on the localStorage to update the list pane. │
+// ╰───────────────────────────────────────────────────────────────────╯
+globalThis.onload = () => {
+	const ref = sessionStorage.getItem("reference");
+	
+	if (ref)
+		document.getElementById("input_estimate").value = sessionStorage
+		.getItem("reference");
+	return (mod.displayCub() && mod.displayAirCub() && mod.countWorks());
+}
+globalThis.onstorage = () => {
+	alert("updated");
+	return (mod.displayCub() && mod.displayAirCub() && mod.countWorks());
+}
+
+
+// ╭────────────────────────────────────────────────────────╮
+// │ Defines the measure of the works selected by the user. │
+// ╰────────────────────────────────────────────────────────╯
+if (!localStorage.getItem("metrica"))
+	localStorage.setItem( "metrica", document.getElementById("metrica").value);
+globalThis.document.getElementById("metrica").addEventListener("change", () =>{
+	if (!localStorage.getItem("metrica")){
+		localStorage.setItem("metrica",
+			document.getElementById("metrica").value
+		);
+	}
+	else {
+		confirm(
+			"Attention! You are going to change the measurement of the artworks."
+		);
+		localStorage.setItem(
+			"metrica",
+			document.getElementById("metrica").value
+		);
+	}
+}, false);
+
+
+// ╭──────────────────────────────────────────────────────╮
+// │ This is the trigger to the "crate" and clear button. │
+// ╰──────────────────────────────────────────────────────╯
+export const crate = () => {
+	mod.crate();
+}
+export const clearAll = () => {
+	const del = confirm("Do you really want to delete the whole list?");
+
+	if (del === true){
+		mod.countWorks();
+		mod.displayAirCub();
+		mod.displayCub();
+		localStorage.clear();
+	}
+	mod.cleanInputs();
+}
