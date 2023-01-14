ae33eb030de8767d5a0f6435c87bbaf12e8b6fd5
Joarell <josephjoarell@gmail.com>
2023-01-13 21:03:22 -0300

    feat (core): adds new roles to delete and get data

--- src/public/front-modules/link.storage.mjs
+++ src/public/front-modules/link.storage.mjs
@@ -4,26 +4,22 @@
 export function createDB() {
 	const dataName = "Estimates";
 	const list = document.getElementById("input_estimate").value;
-	let request = indexedDB.open(dataName);
+	let request = globalThis.indexedDB.open(dataName);
 	const version = request.result.version;
 
 	console.log(version);
 	if(version)
 		request = indexedDB.open(dataName, version + 1);
 	request.onerror = (event) => {
-		alert(`ATTENTION: ${event.error}`);
+		alert(`ATTENTION: ${event.target.errorCode}`);
 	}
-	request.onsuccess = (event) => {
+	request.onupgradeneeded = (event) => {
 		const db = event.target.result;
 		let object;
 	
 		if (!db.objectStoreNames.contains(list)){
 			object = db.createObjectStore(list, {keyPath: "reference"});
-			object.createIndex(
-				"reference",
-				"reference",
-				{ unique: true }
-			);
+			object.createIndex( "reference", "reference", { unique: true });
 		}
 		alert('Done');
 	}
@@ -33,31 +29,93 @@ export function createDB() {
 // ╭─────────────────────────────────╮
 // │ Adds new works to the database. │
 // ╰─────────────────────────────────╯
-export function addNewWork(work, version) {
+export function addNewWork(works, version) {
 	const dataName = "Estimates";
 	const list = document.getElementById("input_estimate").value;
-	const request = indexedDB.open(dataName, version + 1); //take the version with: request.result.version
+	const request = globalThis.indexedDB.open(dataName, version + 1); // TODO: take the version with: request.result.version
 	alert(`Version: ${version}`);
 
 	request.onerror = (event) => {
-		alert(`ERROR: ${event.error}`);
-		createDB();
+		alert(`ERROR: ${event.target.errorCode}`);
 	}
-	request.onsuccess = (event) => {
+	request.onupgradeneeded = (event) => {
 		alert("Go on...");
 		const db = event.target.result;
 		const artWork = db.transaction(list, "readwrite");
 		let objectStore;
 
+		artWork.onerror = (error) => {
+			error.stopPropagation();
+			alert(`ATTENTION: ${error.target.errorCode}`);
+			error.abort();
+		};
 		artWork.oncomplete = () =>{
 			alert('Upgraded!');
 		}
 		objectStore = artWork.objectStore(list);
-		objectStore.add(work);
+		objectStore.add(works);
 
 		alert("Finished!");
-		// request.result.close();
-		// alert("DB was closed");
+		request.result.close();
+		alert("DB was closed");
 		// console.log(request.result.version);
 	}
+}
+
+
+// TODO: test
+// ╭───────────────────────────────────╮
+// │ Delets the estimate on indexedDB. │
+// ╰───────────────────────────────────╯
+export function deleteData(reference) {
+	const request = globalThis.indexedDB.open("Estimates");
+
+	request.onerror = (event) => {
+		alert(`ATTENTION: ${event.target.errorCode}`);
+	}
+	request.onupgradeneeded = (event) => {
+		const db = event.target.result;
+		const deleted = db.transaction(reference, "readwrite")
+		.objectStore("Estimates").delete(reference);
+
+		deleted.onsuccess = () =>{
+			alert(`The ${reference} was success deleted!`);
+		};
+		deleted.onerror = (error) => {
+			alert(`ATTENTION: ${error.target.errorCode}`);
+		};
+		request.result.close();
+	}
+}
+
+
+// TODO: test
+// ╭────────────────────────────────────╮
+// │ Returns the estimate on indexedDB. │
+// ╰────────────────────────────────────╯
+export function gettinData(reference){
+	const request = globalThis.indexedDB.open("Estimate");
+
+	request.onerror = (event) => {
+		alert(`ATTETNTION: ${event.target.errorCode}`);
+	};
+	request.onsuccess = (event) => {
+		const db = event.target.result;
+		const transaction = db.transaction(reference, "readonly");
+
+		transaction.onerror = (error) => {
+			alert(`ATTENTION: ${error.target.erroCode}`);
+		};
+		transaction.onsuccess = () => {
+			const objectstore = transaction.get(reference)
+
+			objectstore.onerror = (error) => {
+				alert(`ATTENTION: ${error.target.erroCode}`);
+			}
+			objectstore.onsuccess = () => {
+				alert(`There you have it: ${objectstore}`);
+			}
+		};
+		request.result.close();
+	};
 }
