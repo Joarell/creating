fc6f998c1dd25e14490880fcce135512e66fb470
Joarell <josephjoarell@gmail.com>
2023-01-16 02:52:51 -0300

    feat : all functions working perfectly

--- src/public/front-modules/link.storage.mjs
+++ src/public/front-modules/link.storage.mjs
@@ -1,121 +1,90 @@
+// ╭────────────────────────────────────────────────────╮
+// │ ╭────────────────────────────────────────────────╮ │
+// │ │ INFO: These are functions to handle indexedDB: │ │
+// │ │                  createDB();                   │ │
+// │ │                 addNewWorks();                 │ │
+// │ │                 deleteData();                  │ │
+// │ │                 gettinData();                  │ │
+// │ ╰────────────────────────────────────────────────╯ │
+// ╰────────────────────────────────────────────────────╯
+
+
 // ╭───────────────────────────────╮
 // │ Creates the browser database. │
 // ╰───────────────────────────────╯
 export function createDB() {
-	const dataName = "Estimates";
-	const list = document.getElementById("input_estimate").value;
-	let request = globalThis.indexedDB.open(dataName);
-	const version = request.result.version;
+	const dataName = "Results";
+	// const list = document.getElementById("input_estimate").value;
+	const request = globalThis.indexedDB.open(dataName);
 
-	console.log(version);
-	if(version)
-		request = indexedDB.open(dataName, version + 1);
 	request.onerror = (event) => {
-		alert(`ATTENTION: ${event.target.errorCode}`);
-	}
+		alert(`ATTENTION! ${event.target.errorCode}`);
+	};
 	request.onupgradeneeded = (event) => {
 		const db = event.target.result;
 		let object;
 	
-		if (!db.objectStoreNames.contains(list)){
-			object = db.createObjectStore(list, {keyPath: "reference"});
-			object.createIndex( "reference", "reference", { unique: true });
-		}
-		alert('Done');
-	}
+		object = db.createObjectStore(dataName, {keyPath: "reference"});
+		object.createIndex( "reference", "reference", { unique: true });
+		console.log('Done!');
+	};
 }
 
 
 // ╭─────────────────────────────────╮
 // │ Adds new works to the database. │
 // ╰─────────────────────────────────╯
-export function addNewWork(works, version) {
-	const dataName = "Estimates";
+export function addNewWorks(works) {
+	const dataName = "Results";
 	const list = document.getElementById("input_estimate").value;
-	const request = globalThis.indexedDB.open(dataName, version + 1); // TODO: take the version with: request.result.version
-	alert(`Version: ${version}`);
+	const request = globalThis.indexedDB.open(dataName);
 
 	request.onerror = (event) => {
 		alert(`ERROR: ${event.target.errorCode}`);
 	}
-	request.onupgradeneeded = (event) => {
-		alert("Go on...");
+	request.onsuccess = (event) => {
 		const db = event.target.result;
-		const artWork = db.transaction(list, "readwrite");
-		let objectStore;
-
-		artWork.onerror = (error) => {
-			error.stopPropagation();
-			alert(`ATTENTION: ${error.target.errorCode}`);
-			error.abort();
-		};
-		artWork.oncomplete = () =>{
-			alert('Upgraded!');
-		}
-		objectStore = artWork.objectStore(list);
-		objectStore.add(works);
-
-		alert("Finished!");
-		request.result.close();
-		alert("DB was closed");
-		// console.log(request.result.version);
+		let object;
+	
+		object = db.transaction("Results", "readwrite").objectStore("Results");
+		object.put(works);
+		gettinData(list);
+		return(globalThis.location.reload());
 	}
 }
 
 
-// TODO: test
 // ╭───────────────────────────────────╮
 // │ Delets the estimate on indexedDB. │
 // ╰───────────────────────────────────╯
 export function deleteData(reference) {
-	const request = globalThis.indexedDB.open("Estimates");
+	const request = globalThis.indexedDB.open("Results");
 
 	request.onerror = (event) => {
 		alert(`ATTENTION: ${event.target.errorCode}`);
 	}
-	request.onupgradeneeded = (event) => {
+	request.onsuccess = (event) => {
 		const db = event.target.result;
-		const deleted = db.transaction(reference, "readwrite")
-		.objectStore("Estimates").delete(reference);
-
-		deleted.onsuccess = () =>{
-			alert(`The ${reference} was success deleted!`);
-		};
-		deleted.onerror = (error) => {
-			alert(`ATTENTION: ${error.target.errorCode}`);
-		};
-		request.result.close();
+		db.transaction("Results", "readwrite")
+			.objectStore("Results").delete(reference);
 	}
 }
 
 
-// TODO: test
 // ╭────────────────────────────────────╮
 // │ Returns the estimate on indexedDB. │
 // ╰────────────────────────────────────╯
 export function gettinData(reference){
-	const request = globalThis.indexedDB.open("Estimate");
+	const request = globalThis.indexedDB.open("Results");
 
 	request.onerror = (event) => {
 		alert(`ATTETNTION: ${event.target.errorCode}`);
 	};
 	request.onsuccess = (event) => {
 		const db = event.target.result;
-		const transaction = db.transaction(reference, "readonly");
-
-		transaction.onerror = (error) => {
-			alert(`ATTENTION: ${error.target.erroCode}`);
-		};
-		transaction.onsuccess = () => {
-			const objectstore = transaction.get(reference)
-
-			objectstore.onerror = (error) => {
-				alert(`ATTENTION: ${error.target.erroCode}`);
-			}
-			objectstore.onsuccess = () => {
-				alert(`There you have it: ${objectstore}`);
-			}
-		};
-		request.result.close();
+		const data = db.transaction("Results", "readonly");
+		const info = data.objectStore("Results").get(reference);
+		
+		return(info.result);
 	};
 }
