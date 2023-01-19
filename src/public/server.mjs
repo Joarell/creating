27a99f542c9e4b722f430cdce21561fc8b14996b
Joarell <josephjoarell@gmail.com>
2023-01-18 22:15:37 -0300

    refactor : adds new server settings

--- src/public/server.mjs
+++ src/public/server.mjs
@@ -1,28 +1,30 @@
 import express from 'express';
-import { router } from './router.mjs';
+// import { router } from './router.mjs';
+// import * as path from 'path';
+
 const app = express();
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+const HOST = '127.0.0.1';
+const port = 3001;
 
 
-// app.use(express.static("loggin"));
-// app.use(express.static("public"));
-
-// app.get("/", (req, res) => {
-// 	res.send(
-// 		"App is out at the moment. We are going return soon. \
-// 		Thank you for you patience!"
-// 	);
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname, '/index.html'));
 // });
 
+
 app.use( express.static("."));
+
 
 app.route("/").get((req, res) => {
 	res.send("We are off-line. :(");
 });
 
-// app.listen(PORT, () => {
-// 	console.log(`App running and listening on port ${PORT}!`)
-// });
 
+app.listen(PORT, () => {
+	console.log(`App running and listening on port ${PORT}!`)
+});
+
+
+// app.listen(port, HOST)
 app.listen(PORT, HOST)
