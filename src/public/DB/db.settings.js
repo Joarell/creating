d75e63522ad3a306f2e809519bd97735b19e2a11
Joarell <josephjoarell@gmail.com>
2023-01-31 04:24:59 -0300

    docs : updates the files location and names

--- src/public/DB/db.settings.js
+++ src/public/DB/db.settings.js
@@ -1,21 +1,17 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
+// const PORT = process.env.PORT || '5432';
+// const USER = process.env.POSTGRES_USER;
+// const PASS = process.env.POSTGRES_PASSWORD;
 
+const connectDB = (port, user, pass) => {
+	const pool = new Pool({
+		host: 'localhost',
+		port: port,
+		user: user,
+		password: pass,
+		database:'web_crater',
+	});
+	return (pool);
+};
 
-const PORT = process.env.PORT;
-const USER = process.env.POSTGRES_USER;
-const PASS = process.env.POSTGRES_PASSWORD;
-const client = new Client({
-	host: 'localhost',
-	port: PORT,
-	user: USER,
-	password: PASS,
-});
-client.connect()
-	.then(() => console.log('connected'))
-	.catch((err) => console.error('connection error', err.stack));
-console.log(client.password);
-
-// const client = new Client();
-// client.connect()
-// 	.then(() => console.log('connected'))
-// 	.catch((err) => console.error('connection error', err.stack));
+module.exports = { connectDB };
