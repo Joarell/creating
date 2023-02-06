19c0ec6fcfe40cd0a2a37dcaee013ab6ffedd400
Joarell <josephjoarell@gmail.com>
2023-02-06 19:53:26 -0300

    perf (update): adds new settings to the app routs

--- src/public/server.mjs
+++ src/public/server.mjs
@@ -1,26 +1,14 @@
 import express from 'express';
-// import { router } from './router.mjs';
-// import * as path from 'path';
+import { router } from './routs/router.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const HOST = 'localhost';
 
 
-app.use( express.static("./www"));
-
-
-// app.route("/").get((req, res) => {
-// 	res.render("./www/index.html");
-// });
-
-app.route("/").get((req, res) => {
-	res.send("We are off-line. :(");
-});
-
-
+app.use(express.json());
+app.use(router);
 app.listen(port, () => {
 	console.log(`App running and listening on port ${port}!`)
 });
-
 app.listen(port, HOST)
