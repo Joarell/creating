99b13ff65c347697bd706fbbfaaacfbeea6310e9
Joarell <josephjoarell@gmail.com>
2023-02-14 10:54:21 -0300

    build (update)!: adds the client module http handler

---
+++ src/public/www/API/bridge.link.web.db.mjs
@@ -0,0 +1,38 @@
+
+
+
+async function postDataFromClientSide (content) {
+	console.log(content);
+	const url = '/insert';
+	await fetch (url, {
+		method: "POST",
+		body: content,
+		headers: {'Content-Type': 'application/json; charset=UTF-8'},
+		cache: 'default'
+	}).catch(err => console.error(`ALERT ${err}`));
+};
+
+
+async function getAllEstimatesReferenceClient () {
+	const url = 'http://localhost:3000/search/estimates';
+	const getter = await fetch(url).then(res => res.json().then(info => info));
+	const codes = getter.map(refe => {
+		const { reference_id } = refe;
+		return ( reference_id );
+	});
+	return (codes);
+};
+
+
+export function getTheCurrentEstimate (estimate) {
+	const contentStorage = JSON.parse(sessionStorage.getItem(estimate));
+	const { reference, list, crates } = contentStorage;
+	const data = {
+		reference: reference,
+		list: list,
+		crates: crates,
+		user_name: 'Jev',
+		user_id: 2
+	};
+	return (postDataFromClientSide(JSON.stringify(data)));
+};
