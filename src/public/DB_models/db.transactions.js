0b602d18c2117c78a62b753c48080bd399c45bad
Joarell <josephjoarell@gmail.com>
2023-02-14 10:48:47 -0300

    feat (api)!: integration to the frontend successed

--- src/public/DB_models/db.transactions.js
+++ src/public/DB_models/db.transactions.js
@@ -3,38 +3,34 @@ require('dotenv').config();
 
 
 async function retriveDataUsers() {
-	pool.connect();
+	await pool.connect();
 	const { rows } = await pool.query('SELECT * FROM craters.users');
-	pool.end();
 	return (rows[0]);
 }
 
 
 async function retriveDataEstimates() {
-	pool.connect();
+	await pool.connect();
 	const { rows } = await pool.query('SELECT * FROM data_solved');
-	pool.end();
 	return (rows);
 }
 
 
 async function updateData (content) {
-	const { reference, total_cub, user_name } = content;
+	const { reference, user_name } = content;
 	const works = JSON.stringify({ "list": content.works }, null, "");
 	const crates = JSON.stringify({ "crates": content.crates }, null, "");
 	const dataUTC = new Date(Date.now()).toLocaleString();
 
-	pool.connect();
+	await pool.connect();
 	try {
 		await pool.query('BEGIN');
 		const up = `UPDATE data_solved SET 
-			works = '${works}',
+			works = '${list}',
 			crates = '${crates}',
-			total_cub = ${total_cub},
 			update_state = '${dataUTC}',
 			updated_by = '${user_name}'
-			WHERE reference_id = '${reference}';
-		`;
+			WHERE reference_id = '${reference}'`;
 		await pool.query(up);
 		await pool.query('COMMIT');
 	}
@@ -46,21 +42,22 @@ async function updateData (content) {
 };
 
 
-// TODO: check with real data from the app
 async function addResultToDataBase(estimate) {
-	const { reference, total_cub, user_name, user_id } = estimate;
-	const works = JSON.stringify({ "list": estimate.works }, null, "");
+	const { reference, user_name, user_id } = estimate;
+	const list = JSON.stringify({ "list": estimate.list }, null, "");
 	const crates = JSON.stringify({ "crates": estimate.crates }, null, "");
 	const dataUTC = new Date(Date.now()).toLocaleString();
 
-	pool.connect();
+	await pool.connect();
 	try {
 		await pool.query('BEGIN');
-		const content = `INSERT INTO data_solved (reference_id, works, crates,
-			total_cub, user_name, user_id, update_state) VALUES ('${reference}',
-			'${works}', '${crates}', ${total_cub}, '${user_name}', ${user_id},
-			'${dataUTC}'
-		)`;
+		const content = `
+			INSERT INTO data_solved 
+			(reference_id, works, crates, user_name, user_id, update_state)
+			VALUES
+			('${reference}', '${list}', '${crates}', '${user_name}', ${user_id},
+			'${dataUTC}')
+		`;
 		await pool.query(content);
 		await pool.query('COMMIT');
 	}
