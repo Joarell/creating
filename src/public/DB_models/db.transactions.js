26534703717a452eec35cca7a92f16e81794926b
Joarell <josephjoarell@gmail.com>
2023-02-16 04:51:51 -0300

    feat (update)!: adds the new module for encriptation

--- src/public/DB_models/db.transactions.js
+++ src/public/DB_models/db.transactions.js
@@ -1,11 +1,15 @@
+
+
+
 const pool = require('./db.settings');
 require('dotenv').config();
+const encription = require('../auth/encriptation.module.js');
 
 
 async function retriveDataUsers() {
 	await pool.connect();
 	const { rows } = await pool.query('SELECT * FROM craters.users');
-	return (rows[0]);
+	return (rows);
 }
 
 
@@ -16,30 +20,30 @@ async function retriveDataEstimates() {
 }
 
 
-async function updateData (content) {
-	const { reference, user_name } = content;
-	const works = JSON.stringify({ "list": content.works }, null, "");
-	const crates = JSON.stringify({ "crates": content.crates }, null, "");
-	const dataUTC = new Date(Date.now()).toLocaleString();
+async function addNewUser (user) {
+	const { name, email, lastName, passFrase, birthday } = user;
+	const criptPass = await encription.passEncriptProcedure (passFrase);
 
+	if (criptPass === 500)
+		return (criptPass);
 	await pool.connect();
 	try {
 		await pool.query('BEGIN');
-		const up = `UPDATE data_solved SET 
-			works = '${list}',
-			crates = '${crates}',
-			update_state = '${dataUTC}',
-			updated_by = '${user_name}'
-			WHERE reference_id = '${reference}'`;
-		await pool.query(up);
+		const content = `
+			INSERT INTO craters.users 
+			(name, last_name, birth_date, email, pass_frase)
+			VALUES
+			('${name}', '${lastName}', '${birthday}', '${email}', '${criptPass}')
+		`;
+		await pool.query(content);
 		await pool.query('COMMIT');
 	}
 	catch (err) {
-		console.error("ATTENTION", err);
+		console.error("ALERT", err);
 		await pool.query('ROLLBACK');
 		throw err;
 	}
-};
+}
 
 
 async function addResultToDataBase(estimate) {
@@ -69,6 +73,32 @@ async function addResultToDataBase(estimate) {
 }
 
 
+async function updateData (content) {
+	const { reference, user_name } = content;
+	const works = JSON.stringify({ "list": content.works }, null, "");
+	const crates = JSON.stringify({ "crates": content.crates }, null, "");
+	const dataUTC = new Date(Date.now()).toLocaleString();
+
+	await pool.connect();
+	try {
+		await pool.query('BEGIN');
+		const up = `UPDATE data_solved SET 
+			works = '${list}',
+			crates = '${crates}',
+			update_state = '${dataUTC}',
+			updated_by = '${user_name}'
+			WHERE reference_id = '${reference}'`;
+		await pool.query(up);
+		await pool.query('COMMIT');
+	}
+	catch (err) {
+		console.error("ATTENTION", err);
+		await pool.query('ROLLBACK');
+		throw err;
+	}
+};
+
+
 async function delEstimate (ref) {
 	const command = `DELETE FROM data_solved WHERE reference_id = '${ref}'`;
 	pool.connect();
@@ -77,6 +107,7 @@ async function delEstimate (ref) {
 }
 
 module.exports = {
+	addNewUser,
 	retriveDataUsers,
 	retriveDataEstimates,
 	addResultToDataBase,
