5b39a42c4a4517d9d2b67ef85c2e93aeb8c0585d
Joarell <josephjoarell@gmail.com>
2023-03-04 12:07:09 -0300

    perf (api)!: spreads the fucntions for better maintaining

--- src/public/middlewares/add.middleware.js
+++ src/public/middlewares/add.middleware.js
@@ -1,59 +1,50 @@
 
 
 
-const db = require('../DB_models/db.transactions');
-const jwt = require('jsonwebtoken');
-const authProcDB = require('../DB_models/db.auth.procedures');
+const db			= require('../DB_models/db.transactions');
+const jwt			= require('jsonwebtoken');
+const authProcDB	= require('../DB_models/db.auth.procedures');
 
 
-function validationContent (data) {
-	const result = data.some((info) => {
-		let test = [undefined, "", null, NaN].includes(info);
-		return (test);
-	});
-	return (result);
+function validationData (data) {
+	const result = data.map((info) => { return((info ?? '') !== ''); });
+	return (result.includes(false));
 };
 
 
 const validationBodyEstimate = async (req, res, next) => {
-	let { body } = req;
-	const { reference, list, crates, user_name, user_id } = body;
-	let content = [reference, list, crates, user_name, user_id];
-	const estimates = await db.retriveDataEstimates();
-	const validation = validationContent(content);
-	const checkEstimate = estimates.find(ref => ref.reference_id === reference);
-	const set = new WeakSet();
+	const { reference, list, crates, user_name, user_id } = req.body;
+	const valid = validationData([reference, list, crates, user_name, user_id]);
 
-	console.log("Test of invalid fields:", validationContent);
-	console.log("Test if the estimate in on DB:", checkEstimate);
-
-	set.add(content);
-	set.add(body);
-	if (validation) {
+	console.log("Test of invalid fields:", valid);
+	if (valid) {
 		return (res.status(206).json({
 			message: `Oops! Please, Check all the information and try again`
 		}));
 	};
-	if (checkEstimate) {
-		return (res.status(409).json({
-			message: `Oops! The estimate already exists!`}));
-	};
 	content = null;
-	body = null;
 	next();
 }
 
 
+const dataEstimateChecker = async (req, res, next) => {
+	const { reference } = req.body;
+	const estimates		= await db.retriveDataEstimates();
+	const checkEstimate = estimates.find(ref => ref.reference_id === reference);
+
+	console.log("Test if the estimate in on DB:", checkEstimate);
+	if (checkEstimate) {
+		return (res.status(409).json({
+			message: `Oops! The estimate already exists!`}));
+	};
+	next();
+};
+
+
 const validationBodyUserAdd = async (req, res, next) => {
 	const { name, email, lastName, passFrase, birthday } = req.body;
-	const prevUsers = await db.retriveDataUsers();
-	const checkUser = prevUsers.find(usr => usr.name === name);
-	let data = validationContent([name, lastName, email, passFrase, birthday]); 
+	const data = validationData([name, lastName, email, passFrase, birthday]); 
 
-	console.log(data);
-	if (checkUser) {
-		return (res.status(409).json({msg: 'The user already exists on DB'}));
-	};
 	if (data) {
 		return (res.status(206).json({
 			message: `Oops! Please, Check all the information and try again`
@@ -63,36 +54,20 @@ const validationBodyUserAdd = async (req, res, next) => {
 };
 
 
-const validationBodyUser = async (req, res, next) => {
-	const { name} = req.body;
+const dataUserChecker = async (req, res, next) => {
+	const { name }	= req.body;
 	const prevUsers = await db.retriveDataUsers();
 	const checkUser = prevUsers.find(usr => usr.name === name);
 
-	if (!checkUser)
+	if (checkUser) {
 		return (res.status(409).json({msg: 'The user already exists on DB'}));
+	};
 	next();
-};
-
-
-const userTokenCheckOut = async (req, res, next) => {
-	const authToken = req.headers['authorization'];
-	const token = authToken && authToken.split(' ')[1];
-	const dbToken = await db.retriveDataUsers();
-	const user = dbToken.find(user => user.auth_token === authToken);
-	console.log(token);
-
-	if(!token)
-		return (res.status(401).json({msg: "Not authorized"}));
-	jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
-		if (err)
-			return (res.status(403).json({msg: "Access denied!"}));
-		next();
-	});
-};
-
+}
 
 module.exports = { 
 	validationBodyUserAdd,
 	validationBodyEstimate,
-	userTokenCheckOut,
+	dataEstimateChecker,
+	dataUserChecker,
 };
