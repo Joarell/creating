be912833c1bea62a31614dbc09883e32ed787cba
Joarell <josephjoarell@gmail.com>
2023-03-04 17:49:16 -0300

    refactor : adds a better formatation to the file

--- src/public/server.js
+++ src/public/server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const router = require('./routs/router.js');
-
-const app = express();
-const port = process.env.PORT || 3000;
-const HOST = 'localhost';
+const express	= require('express');
+const router	= require('./routs/router.js');
+const app		= express();
+const port		= process.env.PORT || 3000;
+const HOST		= 'localhost';
 
 
 app.use(express.json());
@@ -12,3 +11,4 @@ app.listen(port, () => {
 	console.log(`App running and listening on port ${port}!`)
 });
 app.listen(port, HOST)
+
