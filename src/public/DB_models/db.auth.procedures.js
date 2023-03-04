938257e8f990f0f57a8f3fb59ee6d88a3a9620bc
Joarell <josephjoarell@gmail.com>
2023-03-04 12:13:48 -0300

    build : adds the observer study

--- src/public/DB_models/db.auth.procedures.js
+++ src/public/DB_models/db.auth.procedures.js
@@ -8,3 +8,21 @@ async function shiftTokens(user) {
 };
 
 module.exports = { shiftTokens };
+
+const counter = () => {
+	let count = "working";
+	let test = "now!!!";
+	return () => {
+		console.log(count);
+		console.log(test);
+	}
+};
+function obs() {
+	console.log("I'm here");
+};
+
+// const increment = counter();
+// const testing = [increment(), obs()]
+// testing.forEach(ele => {
+// 	return ele;
+// });
