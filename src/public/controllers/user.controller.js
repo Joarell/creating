76ce4edfffa5a49704fcd77985a9240ccc40345d
Joarell <josephjoarell@gmail.com>
2023-03-13 06:41:26 -0300

    refactor (api): adds the token verification functions

--- src/public/controllers/user.controller.js
+++ src/public/controllers/user.controller.js
@@ -1,10 +1,11 @@
 
 
 
-const checker	= require('../auth/user.check.out');
-const db		= require('../DB_models/db.transactions');
-const jwt		= require('jsonwebtoken');
-const tokenMan	= require('../DB_models/db.auth.procedures');
+const checker		= require('../auth/user.check.out');
+const db			= require('../DB_models/db.transactions');
+const jwt			= require('jsonwebtoken');
+const tokenMan		= require('../DB_models/db.auth.procedures');
+const dataTokens	= require('../DB_models/db.tokens.stored');
 
 
 const inserNewUser = async (req, res) => {
@@ -41,11 +42,39 @@ const userLoginValidation = async (req, res) => {
 };
 
 
-function tokensSearcher(tokenPairs, users, id){
-	const user		= users.find(user => user.id === id);
-	const a_token	= tokenPairs[0].includes(user.auth_token);
-	const r_token	= tokenPairs[1].includes(user.refresh_token);
-	
+async function dbSuspiciousTokens (tokens) {
+	const dbTokens			= await dataTokens.retrieveOldTokens();
+	const checkedAuthToken	= dbTokens.includes(tokens[0]);
+	const checkedRefToken	= dbTokens.includes(tokens[1]);
+
+	if ((checkedAuthToken && checkedRefToken) === false)
+		return (false);
+	console.log("TEST1");
+	return (true);
+};
+
+
+async function dbTokensCheckOut (tokens) {
+	const oldTokens			= await dataTokens.retrieveOldTokens();
+	const checkedAuthToken	= oldTokens.includes(tokens[0]);
+	const checkedRefToken	= oldTokens.includes(tokens[1]);
+
+	if ((checkedAuthToken && checkedRefToken) === false)
+		return (false);
+	console.log("TEST2");
+	return (true);
+};
+
+
+async function tokensCheckOut(tokenPairs, users, id){
+	const user				= users.find(user => user.id === id);
+	const a_token			= tokenPairs[0].includes(user.auth_token);
+	const r_token			= tokenPairs[1].includes(user.refresh_token);
+	const checkedTokens		= await dbTokensCheckOut(tokenPairs);
+	const suspiciousTokens	= await dbSuspiciousTokens(tokenPairs);
+
+	if (checkedTokens || suspiciousTokens === true)
+		return(403);
 	if (a_token && r_token === true)
 		return (true);
 	if (a_token || r_token === true)
@@ -55,21 +84,23 @@ function tokensSearcher(tokenPairs, users, id){
 
 
 const userTokenMatch = async( req, res, next) => {
-	const authToken		= req.headers['authorization'].split(' ')[1]
-	const { token }		= req.body;
-	const dbUsers		= await db.retriveDataUsers();
-	const userId		= req.body.id;
-	const result		= tokensSearcher([authToken, token], dbUsers, userId);
+	const authToken	= req.headers['authorization'].split(' ')[1]
+	const { token }	= req.body;
+	const dbUsers	= await db.retriveDataUsers();
+	const userId	= req.body.id;
+	const result	= await tokensCheckOut([authToken, token], dbUsers, userId);
 
 	switch(result) {
 		case true:
 			next();
 			break;
 		case false:
-			tokenMan.storeSuspiciousTokens([authToken, token], req.body.id);
-			return(res.status(401).json({msg: "Token blocked"}));
+			dataTokens.storeSuspiciousTokens([authToken, token], req.body.id);
+			return(res.status(401).json({msg: "User blocked"}));
 		case 404:
 			return (res.status(404).json({msg: "User Not found"}));
+		case 403:
+			return (res.status(403).json({msg: "Suspicious try"}));
 	};
 };
 
@@ -83,15 +114,15 @@ const userTokenExpTime = async (req, res, next) => {
 	if (!token)
 		return (res.status(401).json({msg: "Not authorized"}));
 	jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
-		const time_limit = 10;
+		const time_limit = 0.04;
 		const hourInSec = 3600;
 		const time = ~~ (
 			(new Date(Date.now()) - new Date(err.expiredAt)) / hourInSec
 		) / 1000;
 
-		if (time > time_limit) {
+		console.log(time);
+		if (time > time_limit)
 			return (res.status(403).json({msg: "Access denied!"}));
-		}
 		next();
 	});
 };
