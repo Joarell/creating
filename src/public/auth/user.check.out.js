17fc89721a8cf53c9e69cbb33ccf03aec6adae82
Joarell <josephjoarell@gmail.com>
2023-03-14 16:15:49 -0300

    refactor : updates the function behavior

--- src/public/auth/user.check.out.js
+++ src/public/auth/user.check.out.js
@@ -1,6 +1,9 @@
-const bcrypt = require('bcrypt');
-const db = require('../DB_models/db.transactions');
+
+
+const bcrypt	= require('bcrypt');
+const db		= require('../DB_models/db.transactions');
 require('dotenv').config();
+
 
 async function checkUserAuthDB (userLogin) {
 	const data		= await db.retriveDataUsers();
@@ -8,19 +11,17 @@ async function checkUserAuthDB (userLogin) {
 		if(userLogin.name === user.name)
 			return(user);
 	});
-	console.log(userLogin);
-	console.log(dbUser);
 
 	if(!dbUser)
 		return (404);
 	try {
 		if (await bcrypt.compare(userLogin.passFrase, dbUser.pass_frase))
 			return(200);
-		else
-			return(401);
+		throw (401);
 	}
 	catch (err) {
 		console.error(`WARNING ${err}`);
+		return(401);
 	}
 };
 
