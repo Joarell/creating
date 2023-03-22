9d22b7fb7b65a115854480f070ce2f65ef96a70d
Joarell <josephjoarell@gmail.com>
2023-03-22 06:30:03 -0300

    build (worker): adds new indexedDB worker

---
+++ src/public/www/front-modules/worker.storage.mjs
@@ -0,0 +1,20 @@
+
+
+
+globalThis.onmessage = (estimate) => {
+	const request = globalThis.indexedDB.open("Results");
+
+	request.onerror = (event) => {
+		console.log(`WARNING: ${event.target.errorCode}`);
+	};
+	request.onsuccess = () => {
+		const db = request
+			.result
+			.transaction("Results")
+			.objectStore("Results").get(estimate.data);
+	
+		db.onsuccess = () => {
+			globalThis.postMessage(db.result);
+		};
+	};
+};
