ca071f8b618cfdaee0a01a5ce07f65cbe1a50597
Joarell <josephjoarell@gmail.com>
2023-03-28 12:35:27 -0300

    refactor (update): removes the obsolete code

--- src/public/server.js
+++ src/public/server.js
@@ -1,13 +1,10 @@
 const express	= require('express');
 const router	= require('./routs/router.js');
 const app		= express();
-const port		= process.env.PORT || 3000;
-const HOST		= 'localhost';
+const port		= process.env.PORT || 3001;
 
 
-app.use(express.json());
 app.use(router);
 app.listen(port, () => {
 	console.log(`App running and listening on port ${port}!`)
 });
-app.listen(port, HOST)
