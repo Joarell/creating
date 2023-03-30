1915c97c8ec08d78197e3332071c1e9d9fb8aa7c
Joarell <josephjoarell@gmail.com>
2023-03-30 02:01:28 -0300

    fix (api): updates the securety routes

--- src/public/server.js
+++ src/public/server.js
@@ -4,6 +4,18 @@ const app		= express();
 const port		= process.env.PORT || 3001;
 
 
+app.use((req, res, next ) => {
+	res.setHeader(
+		'Report-TO',
+		'{"group":"csp-endpoint", "max_age":10886400, "endpoints":[{"url":"http://localhost:3000/__cspreport__"}], "include_subdomains":true}'
+	);
+	res.setHeader(
+		'Content-Security-Policy-Report-Only', 
+		"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+	);
+	next();
+});
+
 app.use(router);
 app.listen(port, () => {
 	console.log(`App running and listening on port ${port}!`)
--- src/public/www/server.app.js
+++ src/public/www/server.app.js
@@ -1,11 +1,27 @@
 
 const express	= require('express');
 const app		= express();
-const port		=  3001;
+const port		= process.env.PORT1 || 3001;
 const path		= require('path');
 
 
-app.use(express.static(path.join(__dirname, './www/app')));
+app.use((req, res, next ) => {
+	res.setHeader(
+		'Report-TO',
+		'{"group":"csp-endpoint", "max_age":10886400, "endpoints":[{"url":"http://localhost:3000/__cspreport__"}], "include_subdomains":true}'
+	);
+	res.setHeader(
+		'Content-Security-Policy-Report-Only',
+		"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self' https://fonts.google.com"
+	);
+	next();
+});
+
+app.use(express.static(path.join(__dirname)));
+app.get('/app', (req, res) => {
+	res.status(200).sendFile(path.join(__dirname + './app/index.html'));
+});
+
 app.listen(port, () => {
 	console.log(`App running and listening on port ${port}!`)
 });
