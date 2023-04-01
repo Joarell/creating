d0340114ee9512cb1d798df463450d35f86455ef
Joarell <josephjoarell@gmail.com>
2023-03-31 23:02:23 -0300

    feat (api): adds the serer header policy

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+
 const express	= require('express');
 const router	= require('./routs/router.js');
 const app		= express();
-const port		= process.env.PORT || 3001;
+const port		= process.env.PORT || 3000;
 
 
-app.use((req, res, next) => {
+app.use((req, res, next ) => {
 	res.setHeader(
 		'Report-TO',
 		'{"group":"csp-endpoint", "max_age":10886400, "endpoints":[{"url":"http://localhost:3000/__cspreport__"}], "include_subdomains":true}'
 	);
 	res.setHeader(
-		'Content-Security-Policy-Report-Only', 
-		"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+		'Content-Security-Policy-Report-Only',
+		"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self' https://fonts.google.com"
 	);
 	next();
 });
