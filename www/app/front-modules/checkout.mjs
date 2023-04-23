f0ce9263f40dde8168ee1c0ca54d303efe281f48
Joarell <josephjoarell@gmail.com>
2023-04-23 06:02:44 -0300

    feat (metric): adds the selector handler code

--- www/app/front-modules/checkout.mjs
+++ www/app/front-modules/checkout.mjs
@@ -16,6 +16,7 @@ import { createDB } from './link.storage.mjs';
 // │ Calls to each change on the localStorage to update the list pane. │
 // ╰───────────────────────────────────────────────────────────────────╯
 globalThis.onload = () => {
+	setCheckRadio();
 	browserStoragePrepare();
 };
 
@@ -23,11 +24,61 @@ globalThis.onload = () => {
 globalThis.onstorage = () => {
 	return (mod.displayCub() && mod.displayAirCub() && mod.countWorks());
 }
+// ╭────────────────────────────────────────────────────────╮
+// │ Defines the measure of the works selected by the user. │
+// ╰────────────────────────────────────────────────────────╯
+if (!localStorage.getItem("metrica")) {
+	const metrica = document.getElementById("cm").value;
+	localStorage.setItem("metrica", metrica);
+}
+document.getElementById("units").addEventListener("change", () => {
+	const measure = localStorage.getItem("metrica");
+
+	if (!measure || measure === undefined) {
+		localStorage.setItem("metrica",
+			document.getElementById("cm").value
+		);
+		document.getElementById('cm').checked = true;
+	}
+	else if ( confirm(
+			"Attention! You are going to change the measurement of the works."
+		)){;
+		const storage = localStorage.getItem('metrica');
+		
+		storage === 'centimeters' ? 
+		localStorage.setItem("metrica", "inches") :
+		localStorage.setItem("metrica", 'centimeters');
+		return ;
+	}
+	setCheckRadio();
+}, false);
+
+
+// // ╭──────────────────────────────────────────────────────╮
+// // │ This is the trigger to the "crate" and clear button. │
+// // ╰──────────────────────────────────────────────────────╯
+export const crate = () => {
+	browserStoragePrepare();
+	mod.crate();
+}
+
+export const clearAll = () => {
+	const del = confirm("Do you really want to delete the whole list?");
+
+	if (del === true) {
+		mod.countWorks();
+		mod.displayAirCub();
+		mod.displayCub();
+		localStorage.clear();
+		globalThis.location.reload();
+	}
+	mod.cleanInputs();
+}
 
 
 function browserStoragePrepare() {
 	const ref = localStorage.getItem("refNumb");
-	
+
 	if (ref) {
 		document.getElementById("input_estimate").value = localStorage
 			.getItem("refNumb");
@@ -37,46 +88,15 @@ function browserStoragePrepare() {
 }
 
 
-// ╭────────────────────────────────────────────────────────╮
-// │ Defines the measure of the works selected by the user. │
-// ╰────────────────────────────────────────────────────────╯
-if (!localStorage.getItem("metrica"))
-	localStorage.setItem( "metrica", document.getElementById("metrica").value);
-globalThis.document.getElementById("metrica").addEventListener("change", () => {
-	if (!localStorage.getItem("metrica")){
-		localStorage.setItem("metrica",
-			document.getElementById("metrica").value
-		);
+function setCheckRadio() {
+	const measure = localStorage.getItem("metrica");
+
+	switch (measure) {
+		case 'centimeters':
+			document.getElementById('cm').checked = true;
+			break;
+		case 'inches':
+			document.getElementById('in').checked = true;
+			break;
 	}
-	else {
-		confirm(
-			"Attention! You are going to change the measurement of the works."
-		);
-		localStorage.setItem(
-			"metrica",
-			document.getElementById("metrica").value
-		);
-	}
-}, false);
-
-
-// ╭──────────────────────────────────────────────────────╮
-// │ This is the trigger to the "crate" and clear button. │
-// ╰──────────────────────────────────────────────────────╯
-export const crate = () => {
-	browserStoragePrepare();
-	mod.crate();
-}
-
-export const clearAll = () => {
-	const del = confirm("Do you really want to delete the whole list?");
-
-	if (del === true){
-		mod.countWorks();
-		mod.displayAirCub();
-		mod.displayCub();
-		localStorage.clear();
-		globalThis.location.reload();
-	}
-	mod.cleanInputs();
 }
