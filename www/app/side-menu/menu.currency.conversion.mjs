cbe2b8a2dcc5702c21e1059e0e119f0889433eab
Joarell <josephjoarell@gmail.com>
2023-05-19 02:07:43 -0300

    chore (menu): adds the currency conversion module

---
+++ www/app/side-menu/menu.currency.conversion.mjs
@@ -0,0 +1,33 @@
+
+
+
+function updateCurrency() {
+};
+
+
+function populateCoins() {
+};
+
+
+function convertionCurrency() {
+};
+
+
+function getCurrencyValue() {
+};
+
+
+globalThis.document.getElementById("coin1").addEventListener("change", () => {
+});
+
+
+globalThis.document.getElementById("coin2").addEventListener("change", () => {
+});
+
+
+globalThis.document.getElementById("coin1").addEventListener("input", () => {
+});
+
+
+globalThis.document.getElementById("coin2").addEventListener("input", () => {
+});
