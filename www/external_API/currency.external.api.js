97b4289d3174a26f106589d5d06516b9a7309cfe
Joarell <josephjoarell@gmail.com>
2023-05-20 17:34:14 -0300

    feat (layout): adds the exchange menu setup

--- www/external_API/currency.external.api.js
+++ www/external_API/currency.external.api.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 
 async function getCurrency () {
-	const header = new Headers();
-	header.append("apikey", process.env.API_KEY);
-	const url = `https://api.freecurrencyapi.com/v1/latest?apikey=
-	${process.env.API_KEY}`;
+	const header =	new Headers();
+	header.append("apikey", process.env.API_KEY2);
+	const url =		`https://api.currencybeacon.com/v1/latest?api_key=
+	${process.env.API_KEY2}`;
 	const getterOptions = {
 		method: 'GET',
 		headers: header,
 		redirect: 'follow'
 	};
-	const result = await fetch(url, getterOptions).then(resp => resp.json())
+	const result =	await fetch(url, getterOptions).then(resp => resp.json())
 		.catch(err => (console.error("warning", err)));
 
 	return (result);
