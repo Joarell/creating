ee2ff0c1bc02662c6f6cbb42fcda2503bafdcd9b
Joarell <josephjoarell@gmail.com>
2023-05-22 15:21:53 -0300

    refactor (menu)!: moves the functions to the external modules

--- www/app/side-menu/menu.currency.conversion.mjs
+++ www/app/side-menu/menu.currency.conversion.mjs
@@ -1,77 +1,11 @@
+//    ╭─────────────────────────────────────────────────────────────────────╮
+//    │ ╭─────────────────────────────────────────────────────────────────╮ │
+//    │ │ INFO: Here you are going to find the currency interface caller. │ │
+//    │ ╰─────────────────────────────────────────────────────────────────╯ │
+//    ╰─────────────────────────────────────────────────────────────────────╯
 
-
-
-function currencyName(list) {
-	const fragment = document.createDocumentFragment();
-
-	list.map(name => {
-		const option = document.createElement("option");
-
-		option.textContent = name;
-		fragment.appendChild(option);
-	});
-	return(fragment);
-};
-
-
-async function populateCoins() {
-	const coins =		JSON.parse(localStorage.getItem("currency"))
-	const select1 =		document.getElementById("coin1");
-	const select2 =		document.getElementById("coin2");
-	const coinNames =	Object.keys(coins);
-
-	if (!coins)
-		return("Error");
-	select1.appendChild(currencyName(coinNames));
-	select2.appendChild(currencyName(coinNames));
-};
-
-
-function conversionCurrency(opt1, opt2, val1, val2) {
-	const list =		JSON.parse(localStorage.getItem("currency"));
-	const COMA =		1000;
-	const shiftInput1 =	(Number.parseFloat(val1.value) === list[opt1]);
-	const shiftInput2 =	(Number.parseFloat(val2.value) === list[opt2]);
-
-	if (opt1 === opt2)
-		return(shiftInput1 ? val2.value: val1.value);
-	else if (shiftInput1 && shiftInput2)
-		return (~~((list[opt1] * list[opt2]) * COMA) / COMA);
-	else if (list[opt1] < list[opt2]) {
-		return (shiftInput1 === true ?
-			~~(((val2.value / list[opt2]) * list[opt1]) * COMA) / COMA:
-			~~(((val1.value * list[opt2]) / list[opt1]) * COMA) / COMA
-		);
-	}
-	return (shiftInput2 === true ?
-		~~(((val1.value * list[opt2]) / list[opt1]) * COMA) / COMA:
-		~~(((val2.value / list[opt2]) * list[opt1]) * COMA) / COMA
-	);
-};
-
-
-async function getCurrencyValue() {
-	const url =			'/currency';
-	const getCurrecy =	await fetch(url, {
-		method: "GET",
-		headers: {
-			'Content-Type': 'application/json; charset=UTF-8'
-		},
-	}).then(body => body.json()).catch(err => alert(`CurrencyError: ${err}!`));
-	const { rates } =	getCurrecy.response;
-	const storage =		globalThis.localStorage;
-
-	return(rates && storage.setItem("currency", JSON.stringify(rates)));
-};
-
-
-function setValues (coin, place) {
-	const currency = JSON.parse(localStorage.getItem("currency"));
-
-	if (!currency)
-		return(false);
-	place.value = currency[coin];
-};
+import { getCurrencyValue, populateCoins, setValues, conversionCurrency }
+from './core.currency.mjs';
 
 
 globalThis.document.getElementById("coin-button")
@@ -79,6 +13,7 @@ globalThis.document.getElementById("coin-button")
 	getCurrencyValue();
 	populateCoins();
 });
+
 
 globalThis.document.getElementById("exchange-header")
 	.addEventListener("click", async () => {
@@ -94,6 +29,7 @@ globalThis.document.getElementById("exchange-header")
 	storageCurrency === null ? await getCurrencyValue(): false;
 });
 
+
 globalThis.document.getElementById("coin1")
 	.addEventListener("change", () => {
 	const coin =	document.getElementById("coin1").value;
@@ -103,6 +39,7 @@ globalThis.document.getElementById("coin1")
 	setValues(coin, input);
 });
 
+
 globalThis.document.getElementById("coin2")
 	.addEventListener("change", () => {
 	const coin =	document.getElementById("coin2").value;
@@ -111,6 +48,7 @@ globalThis.document.getElementById("coin2")
 	localStorage.setItem("coin2", JSON.stringify(coin));
 	setValues(coin, input);
 });
+
 
 globalThis.document.getElementById("coin1-input")
 	.addEventListener("input", () => {
@@ -122,6 +60,7 @@ globalThis.document.getElementById("coin1-input")
 	setValues(opt2, value2);
 	value2.value =	conversionCurrency(opt1, opt2, value1, value2);
 });
+
 
 globalThis.document.getElementById("coin2-input")
 	.addEventListener("input", () => {
--- www/app/side-menu/menu.units.mjs
+++ www/app/side-menu/menu.units.mjs
@@ -1,95 +1,11 @@
-//       ╭──────────────────────────────────────────────────────────────╮
-//       │ ╭──────────────────────────────────────────────────────────╮ │
-//       │ │           INFO: Here you find these functions:           │ │
-//       │ │                    centimetersShift()                    │ │
-//       │ │                      inchesShift()                       │ │
-//       │ │                  measureSetupCheckout()                  │ │
-//       │ │                      metersShift()                       │ │
-//       │ │                   resolveConversion()                    │ │
-//       │ │                     unitConvertion()                     │ │
-//       │ ╰──────────────────────────────────────────────────────────╯ │
-//       ╰──────────────────────────────────────────────────────────────╯
+//     ╭──────────────────────────────────────────────────────────────────╮
+//     │ ╭──────────────────────────────────────────────────────────────╮ │
+//     │ │ INFO: Here you are going to find the units interface caller. │ │
+//     │ ╰──────────────────────────────────────────────────────────────╯ │
+//     ╰──────────────────────────────────────────────────────────────────╯
 
 
-function unitConvertion (input1, input2, value1, value2) {
-	const result = (input1 === "centimeters" ?
-		centimetersShift(input1, input2, value1, value2) :
-		input1 === "inches" ?
-			inchesShift(input1, input2, value1, value2) :
-			metersShift(input1, input2, value1, value2)
-	);
-	return (result);
-};
-
-
-function measureSetupCheckout(option1, option2){
-	const checked = {
-		checked1 :option1 === "centimeters" && option2 === "inches",
-		checked2 :option1 === "inches" && option2 === "centimeters",
-		checked3 :option1 === "meters" && option2 === "centimeters",
-	};
-	return (checked);
-};
-
-
-function resolveConversion(input1, input2, unit, type) {
-	const roundDecimal = 1000;
-
-	if (type !== "m")
-		return (input1.value > input2.value ?
-			~~((input1.value / unit) * roundDecimal) / roundDecimal:
-			~~((input2.value * unit) * roundDecimal) / roundDecimal
-		);
-	return (input1.value < input2.value ?
-		~~((input2.value / unit) * roundDecimal) / roundDecimal:
-		~~((input1.value * unit) * roundDecimal) / roundDecimal
-	);
-};
-
-
-function centimetersShift (unit1, unit2, measure1, measure2) {
-	const inches =		2.54;
-	const meters =		0.01;
-	const cmpSetup =	measureSetupCheckout(unit1, unit2);
-
-	if (unit1 === unit2)
-		return (
-			measure1.value > measure2.value ? measure1.value: measure2.value
-		);
-	else if (cmpSetup.checked1)
-		return(resolveConversion(measure1, measure2, inches, "in"));
-	return(resolveConversion(measure1, measure2, meters, "m"));
-};
-
-
-function inchesShift (unit1, unit2, measure1, measure2) {
-	const centimeters = 2.54;
-	const meters =		0.0254;
-	const cmpSetup =	measureSetupCheckout(unit1, unit2);
-
-	if (unit1 === unit2)
-		return (
-			measure1.value > measure2.value ? measure1.value: measure2.value
-		);
-	else if (cmpSetup.checked2)
-		return(resolveConversion(measure1, measure2, centimeters, "cm"));
-	return(resolveConversion(measure1, measure2, meters, "m"));
-};
-
-
-function metersShift (unit1, unit2, measure1, measure2) {
-	const centimeters = 0.01;
-	const inches =		0.0254;
-	const cmpSetup =	measureSetupCheckout(unit1, unit2);
-
-	if (unit1 === unit2)
-		return (
-			measure1.value > measure2.value ? measure1.value: measure2.value
-		);
-	else if (cmpSetup.checked3)
-		return(resolveConversion(measure1, measure2, centimeters, "cm"));
-	return(resolveConversion(measure1, measure2, inches, "in"));
-};
+import { unitConversion } from './core.units.mjs';
 
 
 globalThis.document.getElementById("input-unit1")
@@ -100,7 +16,7 @@ globalThis.document.getElementById("input-unit1")
 	const input2 =		globalThis.document.getElementById("input-unit2");
 
 	input2.value = 0;
-	input2.value = unitConvertion(selected1, selected2, input1, input2);
+	input2.value = unitConversion(selected1, selected2, input1, input2);
 });
 
 
@@ -112,7 +28,7 @@ globalThis.document.getElementById("input-unit2")
 	const input2 =		globalThis.document.getElementById("input-unit2");
 
 	input1.value = 0;
-	input1.value = unitConvertion(selected1, selected2, input1, input2);
+	input1.value = unitConversion(selected1, selected2, input1, input2);
 });
 
 
