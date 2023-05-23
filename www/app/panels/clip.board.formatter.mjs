1d76c17c3c98c076784f4148121a01058bcf5858
Joarell <josephjoarell@gmail.com>
2023-05-23 16:08:58 -0300

    feat (copy): adds the copy sessionStorage emitter

--- www/app/panels/clip.board.formatter.mjs
+++ www/app/panels/clip.board.formatter.mjs
@@ -20,6 +20,7 @@ export function findCratesAndWorks ({ crates }) {
 			);
 		}):
 	false;
+	sessionStorage.setItem("copy", "done!");
 	return(formatterClipBoard(polygons));
 };
 
@@ -32,6 +33,7 @@ export function findCrates ({ crates }) {
 			return (["Crate"].includes(crate[0]) ? crate: false);
 		}):
 	false;
+	sessionStorage.setItem("copy", "done!");
 	return(formatterClipBoard(allCrates));
 };
 
