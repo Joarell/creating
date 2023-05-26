a98fec0fd59bef6db2923e5d8a0026744ccce3da
Joarell <josephjoarell@gmail.com>
2023-05-26 01:10:17 -0300

    refactor (DB): removes the fetch DB function

--- www/app/front-modules/bridge.link.web.db.mjs
+++ www/app/front-modules/bridge.link.web.db.mjs
@@ -17,21 +17,6 @@ async function postDataFromClientSide (content) {
 };
 
 
-async function getAllEstimatesReferenceClient () {
-	const url		= '/search/estimates';
-	const getter	= await fetch(url)
-		.then(res => res
-		.json()
-		.then(info => info)
-	);
-	const codes		= getter.map(refe => {
-		const { reference_id } = refe;
-		return (reference_id);
-	});
-	return (codes);
-};
-
-
 export function saveTheCurrentEstimate (estimate) {
 	const contentStorage = JSON.parse(sessionStorage.getItem(estimate));
 	const { reference, list, crates } = contentStorage;
