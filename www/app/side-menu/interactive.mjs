3f73917cc87c08eb614ad769179cc74068ef2842
Joarell <josephjoarell@gmail.com>
2023-06-08 05:10:08 -0300

    feat (mobile): introduces the menu selected behavior

--- www/app/side-menu/interactive.mjs
+++ www/app/side-menu/interactive.mjs
@@ -12,8 +12,8 @@ globalThis.document.querySelector(".accordion")
 
 
 function toggleAccordion(clicked) {
-	const buttons = clicked.parentElement.querySelectorAll("button");
-	const panel = clicked.parentElement.querySelectorAll(".menu__input");
+	const buttons =	clicked.parentElement.querySelectorAll("button");
+	const panel =	clicked.parentElement.querySelectorAll(".menu__input");
 
 	buttons.forEach(button => {
 		button.setAttribute("aria-expanded", false);
@@ -29,3 +29,34 @@ function openPanel(panel) {
 	panel.querySelector("button").setAttribute("aria-expanded", true);
 	panel.querySelector(".menu__input").setAttribute("aria-hidden", false);
 };
+
+
+globalThis.document.querySelector(".IO__press-mobile")
+	.addEventListener("click", (element) => {
+	const { id } =		element.target;
+	const menuOpts =	document.querySelector(".fan-options");
+	const menu =		document.querySelector(".get-estimate");
+
+	if (id === "menu-options" && menuOpts.ariaHidden) {
+		menuOpts.setAttribute("aria-expanded", true);
+		menuOpts.setAttribute("aria-hidden", false);
+	}
+	else if(id === "fetch-mob") {
+		menu.setAttribute("aria-hidden", false);
+		menu.setAttribute("aria-expanded", true);
+		setTimeout(() => {
+			document.getElementById("search").click();
+		}, 1000);
+		setTimeout(() => {
+			globalThis.scroll({top: 10000, behavior: "smooth"});
+		}, 2000);
+		menu.setAttribute("aria-hidden", false);
+		menuOpts.setAttribute("aria-expanded", false);
+		menuOpts.setAttribute("aria-hidden", true);
+	}
+	else {
+		menuOpts.setAttribute("aria-expanded", false);
+		menuOpts.setAttribute("aria-hidden", true);
+	}
+	console.log(element.target.id);
+}, true);
