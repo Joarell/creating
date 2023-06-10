055f9af922ff0ce1237b6d74421627728914467b
Joarell <josephjoarell@gmail.com>
2023-06-09 21:59:08 -0300

    feat (mode): adds the dark mode module - adds tiny changes to the side menu - adds the light and dark classes

---
+++ www/app/front-modules/mode.color.mjs
@@ -0,0 +1,23 @@
+
+
+globalThis.document.querySelector(".side-menu")
+	.addEventListener("change", (color) => {
+	const mode = color.target.value;
+
+	localStorage.setItem("mode", mode);
+	changeMode(mode);
+});
+
+
+
+function changeMode (color) {
+	const body = document.body.classList;
+
+	body.remove("light-mode");
+	body.remove("dark-mode");
+	return (
+		color === "dark" ?
+			body.add("dark-mode"):
+			body.add("light-mode")
+	);
+};
