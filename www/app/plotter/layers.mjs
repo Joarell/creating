6ae486779561f5df110671c1caf8a14595fe536f
Joarell <josephjoarell@gmail.com>
2023-06-21 19:50:12 -0300

    feat (plotter): adds the first work with code properly

--- www/app/plotter/layers.mjs
+++ www/app/plotter/layers.mjs
@@ -2,14 +2,6 @@
 
 
 
-element.appendChild(rect);
-element.appendChild(text);
-
-
-
-console.log(screen);
-
-
 plotter([200, 100]);
 
 
@@ -19,55 +11,71 @@ function plotter(crate) {
 	const screen =	globalThis.screen.availWidth;
 	const DESKTOP = 1920;
 	let appView;
+	let work1 = {x: 100, y: 100}
+	let work2 = {x: 100, y: 100}
 
 	screen >= DESKTOP ?
 		appView = deskTopView(crate):
 		appView = mobileView(crate);
 	draw.setAttribute("width", appView.x);
 	draw.setAttribute("height", appView.y);
-	draw.appendChild(arranger(appView, appView));
+	draw.appendChild(arranger(work1, work1, appView, crate));
 	view.appendChild(draw);
 }
 
 
-function arranger({ x }, { y }) {
+function arranger({ x }, { y }, pixLayer, crateLayer) {
 	const element =	document.createDocumentFragment();
+	const sizeX =	proportion(x, pixLayer.x, crateLayer[0]);
+	const sizeY =	proportion(y, pixLayer.y, crateLayer[1]);
+	let code = "TEST";
 
-	element.appendChild(positionWork(x, y, code));
-	retun(element);
+	element.appendChild(workPosition(x, y, pixLayer, crateLayer));
+	element.appendChild(textOnCenter(sizeX, sizeY, code));
+	return(element);
 }
 
 
-function positionWork(x, y, space) {
+function workPosition(x, y, size, space) {
 	const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-	let code = "TEST";
+	const sizeX =	proportion(x, size.x, space[0]);
+	const sizeY =	proportion(y, size.y, space[1]);
+	const PAD =		4;
 
-	rect.setAttribute("x", 4);
-	rect.setAttribute("y", 4);
+	rect.setAttribute("x", PAD);
+	rect.setAttribute("y", PAD);
 	rect.setAttribute("rx", 5);
 	rect.setAttribute("ry", 5);
 	rect.setAttribute("fill", "#03e9f4");
-	rect.setAttribute("width", 300);
-	rect.setAttribute("height", 100);
-	rect.appendChild(textOnCenter(x, y, code));
+	rect.setAttribute("width", sizeX);
+	rect.setAttribute("height", sizeY - 10);
+	return(rect);
 }
 
 
-function textOnCenter(x, y, code) {
+function proportion(val, pixArea, layer) {
+	return (
+		(val / layer) * pixArea
+	);
+}
+
+
+function textOnCenter(x, y, id) {
 	const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
 	const COL = "#96979C"
 
 	text.setAttribute("x", x / 2);
 	text.setAttribute("y", y / 2);
-	text.setAttribute("fill", COL);
-	text.innerHTML = code;
+	text.setAttribute("fill", "white");
+	text.innerHTML = id;
 	return (text);
 }
 
 
 function deskTopView(sizes) {
 	const MAXSIZE =	900;
-	const UPLEFT =	4;
+	const PADUP =	4;
+	const PADDOWN =	10;
 	let layerLength;
 	let layerHeight;
 
@@ -79,14 +87,15 @@ function deskTopView(sizes) {
 		layerHeight = MAXSIZE;
 		layerHeight = (sizes[0] / sizes[1]) * MAXSIZE;
 	}
-	return ({x: layerLength, y: layerHeight});
+	return ({x: layerLength + PADUP, y: layerHeight + PADDOWN});
 };
 
 
 function mobileView(sizes) {
 	const MOBILEWIDTH =		395;
 	const MOBILEHEIGHT =	198;
-	const UPLEFT =			4;
+	const PADUP =			4;
+	const PADDOWN =			10;
 	let layerLength;
 	let layerHeight;
 
@@ -98,5 +107,5 @@ function mobileView(sizes) {
 		layerHeight = MOBILEHEIGHT;
 		layerHeight = (sizes[0] / sizes[1]) * MOBILEWIDTH;
 	}
-	return ({x: layerLength, y: layerHeight});
+	return ({x: layerLength + PADUP, y: layerHeight + PADDOWN});
 };
