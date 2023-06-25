595e6571d869d74f570fe368cf6e6212fc8678c8
Joarell <josephjoarell@gmail.com>
2023-06-25 03:15:32 -0300

    style (update): adds the correct tabs variable declaration

--- www/app/front-modules/Art.class.def.mjs
+++ www/app/front-modules/Art.class.def.mjs
@@ -3,75 +3,76 @@
 // ╰────────────────────────────────────────────────────────────╯
 
 
+// TODO: create the regex function applying the DRY principle.
 export default class ArtWork {
 	constructor (code, x, z, y) {
-		this.code = code;
-		this.x = x;
-		this.z = z;
-		this.y = y;
-	}
+		this.code =		code;
+		this.x =		x;
+		this.z =		z;
+		this.y =		y;
+	};
 	get cubeAir () {
 		return (this.cAir());
-	}
+	};
 	cAir () {
-		const cons = 6000;
-		const x = parseInt(this.x);
-		const z = parseInt(this.z);
-		const y = parseInt(this.y);
-		const regx = /[0-9]{1,3}/.test(x);
-		const regz = /[0-9]{1,3}/.test(z);
-		const regy = /[0-9]{1,3}/.test(y);
+		const CUBICAIR =	6000;
+		const x =			parseInt(this.x);
+		const z =			parseInt(this.z);
+		const y =			parseInt(this.y);
+		const regx =		/[0-9]{1,3}/.test(x);
+		const regz =		/[0-9]{1,3}/.test(z);
+		const regy =		/[0-9]{1,3}/.test(y);
 
 		if (regx && regz && regy)
-			return ((x * z * y) / cons);
+			return ((x * z * y) / CUBICAIR);
 		return (false);
-	}
+	};
 	get cub () {
 		if (this.cubed() === false)
 			return (false);
 		return (Math.floor(this.cubed() * 1000) / 1000);
-	}
+	};
 	cubed () {
-		const cmToM = 1000000;
-		const x = parseInt(this.x);
-		const z = parseInt(this.z);
-		const y = parseInt(this.y);
-		const regx = /[0-9]{1,3}/.test(x);
-		const regz = /[0-9]{1,3}/.test(z);
-		const regy = /[0-9]{1,3}/.test(y);
+		const CMTOM =	1_000_000;
+		const x =		parseInt(this.x);
+		const z =		parseInt(this.z);
+		const y =		parseInt(this.y);
+		const regx =	/[0-9]{1,3}/.test(x);
+		const regz =	/[0-9]{1,3}/.test(z);
+		const regy =	/[0-9]{1,3}/.test(y);
 		
 		if (regx && regz && regy)
-			return ((x * z * y) / cmToM);
+			return ((x * z * y) / CMTOM);
 		return (false);
-	}
+	};
 	get vector () {
 		const x = parseFloat(this.x);
 		const z = parseFloat(this.z);
 		const y = parseFloat(this.y);
 
 		return ([this.code, x, z, y]);
-	}
+	};
 	// TODO: not tested yet.
-	conversion(metric){
-		const inch = 0.39;
-		const dimensions = [];
-		const x = parseFloat(this.x);
-		const z = parseFloat(this.z);
-		const y = parseFloat(this.y);
+	conversion (metric) {
+		const INCH =		0.39;
+		const dimensions =	[];
+		const x =			parseFloat(this.x);
+		const z =			parseFloat(this.z);
+		const y =			parseFloat(this.y);
 
 		if (metric === "cm"){
-			dimensions.push(Math.floor((x / inch) * 100) / 100);
-			dimensions.push(Math.floor((z / inch) * 100) / 100);
-			dimensions.push(Math.floor((y / inch) * 100) / 100);
+			dimensions.push(Math.floor((x / INCH) * 100) / 100);
+			dimensions.push(Math.floor((z / INCH) * 100) / 100);
+			dimensions.push(Math.floor((y / INCH) * 100) / 100);
 			return (dimensions);
 		}
 		else if (metric === "in"){
-			dimensions.push(Math.floor((x * inch) * 100) / 100);
-			dimensions.push(Math.floor((z * inch) * 100) / 100);
-			dimensions.push(Math.floor((y * inch) * 100) / 100);
+			dimensions.push(Math.floor((x * INCH) * 100) / 100);
+			dimensions.push(Math.floor((z * INCH) * 100) / 100);
+			dimensions.push(Math.floor((y * INCH) * 100) / 100);
 			return (dimensions);
 		}
 		else
 			return("incorrect value");
-	}
-}
+	};
+};
