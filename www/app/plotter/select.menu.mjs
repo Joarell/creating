5c76949305692150f9be58dd07226a954fae819c
Joarell <josephjoarell@gmail.com>
2023-07-04 00:28:44 -0300

    perf (update): setts the options to the select menu

--- www/app/plotter/select.menu.mjs
+++ www/app/plotter/select.menu.mjs
@@ -1,5 +1,16 @@
 
 
-
 export function populateOptions(crates) {
+	const select =	document.getElementById('selected-crate');
+	const unit =	localStorage
+		.getItem("metrica") === 'cm - centimeters' ? 'cm' : 'in';
+
+	select.innerHTML += crates.map((crate, i) => {
+		if(crate[0] === 'Crate')
+			return(`
+				<option>
+					Crate ${i}: ${crate[1]} x ${crate[2]} x ${crate[3]} - ${unit}
+				</option>
+			`);
+	}, 0);
 };
