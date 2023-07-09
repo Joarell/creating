16fcad65a6a0fb42f80e243f41601f82eb5f9a1e
Joarell <josephjoarell@gmail.com>
2023-07-08 21:38:37 -0300

    feat (test): updates the class file path

--- tests/frontEnd.test.mjs
+++ tests/frontEnd.test.mjs
@@ -1,73 +1,81 @@
 "use strict"
 
 // impmort { describe, it, before, beforeEach, after, afterEach } from 'node:test';
-import ArtWork from '../public/front-modules/classes.def.mjs';
+import ArtWork from '../www/app/front-modules/Art.class.def.mjs';
 import { describe, it } from 'node:test';
 import assert from 'node:assert'
 
 
 describe("Testing display cub calc property from the class Artwork", () => {
-	it("It Should return the cub calc of the work with the value 0.03 variable value: ArtWork \"00001\", \"100\", \"3\", \"100\"", () => {
+	it("TEST-1: it should return the cub calc of the work.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "100", "3", "100"));
 		const expected = 0.03;
 		const current = tWork[0].cub;
+
 		assert.deepStrictEqual(current, expected);
 	});
-	console.log("Done!");
-	it("It Should return false variable value: ArtWork \"00001\", \"\", \"3\", \"100\"", () => {
+
+	it("TEST-2: it should return false.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "", "3", "100"));
 		const expected = false;
 		const current = tWork[0].cub;
+
 		assert.deepStrictEqual(current, expected);
 	});
 
-	it("It Should return false variable value: ArtWork \"00001\", \"\", \"\", \"100\"", () => {
+	it("TEST-3: it should return false.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "", "", "100"));
 		const expected = false;
 		const current = tWork[0].cub;
+
 		assert.deepStrictEqual(current, expected);
 	});
 
-	it("It Should return false variable value: ArtWork \"00001\", \"\", \"\", \"\"", () => {
+	it("TEST-4: it should return false.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "", "", ""));
 		const expected = false;
 		const current = tWork[0].cub;
+
 		assert.deepStrictEqual(current, expected);
 	});
 	
-	it("It Should return the cub calc of the work with the value 0.03 variable value: ArtWork \"00001\", \"100\", \"3\", \"100\"", () => {
+	it("TEST-5: it should return the cub calc.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "100", "3", "100"));
 		const expected = 5;
 		const current = tWork[0].cubeAir;
+
 		assert.deepStrictEqual(current, expected);
 	});
 
-	it("It Should return false variable value: ArtWork \"00001\", \"\", \"3\", \"100\"", () => {
+	it("TEST-6: it should return false.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "", "03", "100"));
 		const expected = false;
 		const current = tWork[0].cubeAir;
+
 		assert.deepStrictEqual(current, expected);
 	});
 
-	it("It Should return false variable value: ArtWork \"00001\", \"\", \"\", \"100\"", () => {
+	it("TEST-7: it should return false.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "", "", "100"));
 		const expected = false;
 		const current = tWork[0].cubeAir;
+
 		assert.deepStrictEqual(current, expected);
 	});
 
-	it("It Should return false variable value: ArtWork \"00001\", \"\", \"\", \"\"", () => {
+	it("TEST-8: it should return false.", () => {
 		const tWork = [];
 		tWork.push(new ArtWork("00001", "", "", ""));
 		const expected = false;
 		const current = tWork[0].cubeAir;
+
 		assert.deepStrictEqual(current, expected);
 	});
 });
