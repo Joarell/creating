55b9235476247538f6345b7bb510fec931e832c9
Joarell <josephjoarell@gmail.com>
2023-07-08 21:20:37 -0300

    feat (update): adds the string checkin

--- tests/extra.math.test.mjs
+++ tests/extra.math.test.mjs
@@ -1,55 +1,77 @@
-import * as extra from "../public/core/extras.math.mjs";
+import * as extra from "../www/app/core/extras.math.mjs";
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
-import ArtWork from "../public/front-modules/classes.def.mjs";
 
 
 describe ("Extra math tests to verify the correct returns", () => {
 	it("Test-1: returns the \"a\" value in the pitagoras equation.", () => {
-		let a = 0;
-		let b = 100;
-		let c = 90;
-		const current = extra.pitagoras(a, b, c);
-		const expected = 134.53;
+		let a =				0;
+		let b =				100;
+		let c =				90;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	134.53;
 		
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-2: returns the \"b\" value in the pitagoras equation.", () => {
-		let a = 100;
-		let b = 0;
-		let c = 90;
-		const current = extra.pitagoras(a, b, c);
-		const expected = 43.58;
+		let a =				100;
+		let b = 			0;
+		let c = 			90;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	43.58;
 		
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-3: returns the \"c\" value in the pitagoras equation.", () => {
-		let a = 100;
-		let b = 90;
-		let c = 0;
-		const current = extra.pitagoras(a, b, c);
-		const expected = 43.58;
+		let a =				100;
+		let b = 			90;
+		let c = 			0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	43.58;
 
 		assert.strictEqual(current, expected);
 	});
 
-	it("Test-4: converts the centimeters sizes to inches.", () => {
-		const current = extra.convertCmToIn(
-			[new ArtWork("000111", "100", "10", "100")]
-		);
-		const expected = [new ArtWork("000111", "39", "3.9", "39")];
+	it("Test-4: returns error sending a string to pitagoras.", () => {
+		let a =				"100";
+		let b = 			90;
+		let c = 			0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
 
-		assert.strict(current, expected);
+		assert.strictEqual(current, expected);
 	});
 
-	it("Test-5: converts the iches sizes to centimeters.", () => {
-		const current = extra.convertInToCm(
-			[new ArtWork("000111", "39", "3.9", "39")]
-		);
-		const expected = [new ArtWork("000111", "100", "10", "100")];
+	it("Test-5: returns error sending a string to pitagoras.", () => {
+		let a =				100;
+		let b = 			"90";
+		let c = 			0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
 
-		assert.strict(current, expected);
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-6: returns error sending a string to pitagoras.", () => {
+		let a =				100;
+		let b = 			90;
+		let c = 			"0";
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-7: returns error sending a string to pitagoras.", () => {
+		let a =				"100";
+		let b = 			"90";
+		let c = 			"0";
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
 	});
 });
+
