0a6fffe4bf5b7167b7756b2f76d6fa54aeb4bfe1
Joarell <josephjoarell@gmail.com>
2023-07-10 19:56:12 -0300

    docs (test): adds new test for string handlers

--- tests/extra.math.test.mjs
+++ tests/extra.math.test.mjs
@@ -1,55 +1,196 @@
-import * as extra from "../public/core/extras.math.mjs";
+import { artWorksList, artList } from "./mock.artworks.mjs";
+import * as extra from "../www/app/core/extras.math.mjs";
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
-import ArtWork from "../public/front-modules/classes.def.mjs";
 
 
 describe ("Extra math tests to verify the correct returns", () => {
 	it("Test-1: returns the \"a\" value in the pitagoras equation.", () => {
-		let a = 0;
-		let b = 100;
-		let c = 90;
-		const current = extra.pitagoras(a, b, c);
-		const expected = 134.53;
+		let a =				0;
+		let b =				100;
+		let c =				90;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	134.53;
 		
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-2: returns the \"b\" value in the pitagoras equation.", () => {
-		let a = 100;
-		let b = 0;
-		let c = 90;
-		const current = extra.pitagoras(a, b, c);
-		const expected = 43.58;
+		let a =				100;
+		let b =				0;
+		let c =				90;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	43.58;
 		
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-3: returns the \"c\" value in the pitagoras equation.", () => {
-		let a = 100;
-		let b = 90;
-		let c = 0;
-		const current = extra.pitagoras(a, b, c);
-		const expected = 43.58;
+		let a =				100;
+		let b =				90;
+		let c =				0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	43.58;
 
 		assert.strictEqual(current, expected);
 	});
 
-	it("Test-4: converts the centimeters sizes to inches.", () => {
-		const current = extra.convertCmToIn(
-			[new ArtWork("000111", "100", "10", "100")]
-		);
-		const expected = [new ArtWork("000111", "39", "3.9", "39")];
+	it("Test-4: returns the \"c\" value in the pitagoras equation.", () => {
+		let a =				100;
+		let b =				90;
+		let c =				0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	43.58;
 
-		assert.strict(current, expected);
+		assert.strictEqual(current, expected);
 	});
 
-	it("Test-5: converts the iches sizes to centimeters.", () => {
-		const current = extra.convertInToCm(
-			[new ArtWork("000111", "39", "3.9", "39")]
-		);
-		const expected = [new ArtWork("000111", "100", "10", "100")];
+	it("Test-5: returns false from pitagoras equation.", () => {
+		let a =				"100";
+		let b =				90;
+		let c =				0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
 
-		assert.strict(current, expected);
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-6: returns false from pitagoras equation.", () => {
+		let a =				100;
+		let b =				"90";
+		let c =				0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-7: returns false from pitagoras equation.", () => {
+		let a =				100;
+		let b =				90;
+		let c =				"0";
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-8: returns false from pitagoras equation.", () => {
+		let a =				"100";
+		let b =				"90";
+		let c =				"0";
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-9: returns false from pitagoras equation.", () => {
+		let a =				100;
+		let b =				"90";
+		let c =				"0";
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-10: returns false from pitagoras equation.", () => {
+		let a =				"100";
+		let b =				90;
+		let c =				"0";
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-11: returns false from pitagoras equation.", () => {
+		let a =				"100";
+		let b =				"90";
+		let c =				0;
+		const current =		extra.pitagoras(a, b, c);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-12: returns false from cubing.", () => {
+		let a =				"100";
+		let b =				"90";
+		let c =				"0";
+		const current =		extra.cubing(["code", a, b, c]);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-13: returns false from cubing.", () => {
+		let a =				100;
+		let b =				"90";
+		let c =				"0";
+		const current =		extra.cubing(["code", a, b, c]);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-14: returns false from cubing.", () => {
+		let a =				"100";
+		let b =				90;
+		let c =				"0";
+		const current =		extra.cubing(["code", a, b, c]);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-15: returns false from cubing.", () => {
+		let a =				"100";
+		let b =				"90";
+		let c =				0;
+		const current =		extra.cubing(["code", a, b, c]);
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-16: returns false from cubing.", () => {
+		let a =				100;
+		let b =				5;
+		let c =				100;
+		const current =		extra.cubing(["code", a, b, c]);
+		const expected =	0.05;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-17: returns the biggest work on the list.", () => {
+		const current =		extra.big_work(artList()); 
+		// ['88800', 70, 70, 70] || ['230202', 70, 70, 70]
+		const expected =	0.343;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-18: returns false from bigWork.", () => {
+		const current =		extra.big_work(""); 
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-19: returns false from bigWork.", () => {
+		const current =		extra.big_work([["TEST"]]); 
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-20: returns false from bigWork.", () => {
+		const current =		extra.big_work("TEST"); 
+		const expected =	false;
+
+		assert.strictEqual(current, expected);
 	});
 });
