7288e4a3b6cacb42643c013d3d6df69138c8823b
Joarell <josephjoarell@gmail.com>
2023-07-11 15:50:37 -0300

    chore (test): adds new tests to the ArtWork class

--- tests/art.class.test.mjs
+++ tests/art.class.test.mjs
@@ -1,28 +1,94 @@
-
 import ArtWork from "../www/app/front-modules/Art.class.def.mjs";
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
 
 
 describe("Art work class tests to verify all methods implemented", () => {
-	it("Test-1: returns the cubic calculation made by air companies.", () => {
+	it("Test-1: returns a artwork object class.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		typeof(art);
+		const expected =	'object';
+
+		assert.deepEqual(current, expected);
 	});
 
-	it("Test-4: converts the centimeters sizes to inches.", () => {
-		const current = extra.convertCmToIn(
-			[new ArtWork("000111", "100", "10", "100")]
-		);
-		const expected = [new ArtWork("000111", "39", "3.9", "39")];
+	it("Test-2: converts the string sizes to integers - x.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		Number.isSafeInteger(art.x);
+		const expected =	true;
 
-		assert.strict(current, expected);
+		assert.strictEqual(current, expected);
 	});
 
-	it("Test-5: converts the iches sizes to centimeters.", () => {
-		const current = extra.convertInToCm(
-			[new ArtWork("000111", "39", "3.9", "39")]
-		);
-		const expected = [new ArtWork("000111", "100", "10", "100")];
+	it("Test-3: converts the string sizes to integers - z.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		Number.isSafeInteger(art.z);
+		const expected =	true;
 
-		assert.strict(current, expected);
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-4: converts the string sizes to integers - y.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		Number.isSafeInteger(art.y);
+		const expected =	true;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-5: returns the cubic air calculation.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		art.cAir();
+		const expected =	2.083;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-6: returns the cubic calculation.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		art.cubed();
+		const expected =	0.013;
+
+		assert.strictEqual(current, expected);
+	});
+
+	it("Test-7: returns the conversion sizes - cm to in.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		art.conversion("in");
+		const expected =	[127, 12.7, 127];
+
+		assert.deepEqual(current, expected);
+	});
+	
+	it("Test-8: returns the conversion sizes - in to cm.", () => {
+		const art =			new ArtWork("098", "127", "12.7", "127");
+		const current =		art.conversion("cm");
+		const expected =	[50, 5, 50];
+
+		assert.deepEqual(current, expected);
+	});
+
+	it("Test-9: returns the inch conversion sizes.", () => {
+		const art =			new ArtWork("098", "127", "12.7", "127");
+		const current =		art.conversion();
+		const expected =	false;
+
+		assert.deepEqual(current, expected);
+	});
+
+	it("Test-10: returns the array sizes.", () => {
+		const art =			new ArtWork("098", "50", "5", "50");
+		const current =		art.arr();
+		const expected =	['098', 50, 5, 50];
+
+		assert.deepEqual(current, expected);
+	});
+
+	it("Test-11: returns the array sizes.", () => {
+		const art =			new ArtWork("098", "127", "12.7", "127");
+		const current =		art.arr();
+		const expected =	['098', 127, 12.7, 127];
+
+		assert.deepEqual(current, expected);
 	});
 });
