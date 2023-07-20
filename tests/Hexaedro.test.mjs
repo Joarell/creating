f556bbc2b676fd989f29df01bc3ec9c1320f40b2
Joarell <josephjoarell@gmail.com>
2023-07-20 13:07:07 -0300

    refactor (update): adds the new calls name - adds the private fields to the class

--- tests/Hexaedro.test.mjs
+++ tests/Hexaedro.test.mjs
@@ -1,51 +1,51 @@
-import Pipedo from'../www/app/core2/Pipedo.class.mjs';
+import Hexaedro from'../www/app/core2/Hexaedro.class.mjs';
 import { describe, it } from 'node:test';
 import assert from 'node:assert';
 
 
-describe("These are test to the Pipedo class.", () => {
+describe("These are test to the Hexaedro class.", () => {
 	it("Test-1: returns the paralellepiped object", () => {
-		const pipedo =		new Pipedo("100", "5", "100");
-		const current =		pipedo instanceof Pipedo;
+		const hexa =		new Hexaedro("100", "5", "100");
+		const current =		hexa instanceof Hexaedro;
 		const expected =	true;
 
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-2: returns an int to the 'x' measure.", () => {
-		const pipedo =		new Pipedo("100", "5", "100");
-		const current =		Number.isSafeInteger(pipedo.x);
+		const hexa =		new Hexaedro("100", "5", "100");
+		const current =		Number.isSafeInteger(hexa.x);
 		const expected =	true;
 
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-3: returns an int to the 'z' measure.", () => {
-		const pipedo =		new Pipedo("100", "5", "100");
-		const current =		Number.isSafeInteger(pipedo.z);
+		const hexa =		new Hexaedro("100", "5", "100");
+		const current =		Number.isSafeInteger(hexa.z);
 		const expected =	true;
 
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-4: returns an int to the 'y' measure.", () => {
-		const pipedo =		new Pipedo("100", "5", "100");
-		const current =		Number.isSafeInteger(pipedo.y);
+		const hexa =		new Hexaedro("100", "5", "100");
+		const current =		Number.isSafeInteger(hexa.y);
 		const expected =	true;
 
 		assert.strictEqual(current, expected);
 	});
 
 	it("Test-5: returns false when pass empty data to the class.", () => {
-		const current =		new Pipedo(" ");
+		const current =		new Hexaedro(" ");
 		const error =		"Please, provide a correct x, z or y value.";
 		const expected =	new TypeError(error);
 
 		assert.deepStrictEqual(current, expected);
 	});
 
-	it("Test-6: returns false when pass only x to Pipedo class.", () => {
-		const current =		new Pipedo("100");
+	it("Test-6: returns false when pass only x to Hexaedro class.", () => {
+		const current =		new Hexaedro("100");
 		const error =		"Please, provide a correct x, z or y value.";
 		const expected =	new TypeError(error);
 
@@ -53,7 +53,7 @@ describe("These are test to the Pipedo class.", () => {
 	});
 
 	it("Test-7: returns false when pass only one value.", () => {
-		const current =		new Pipedo("100", "5");
+		const current =		new Hexaedro("100", "5");
 		const error =		"Please, provide a correct x, z or y value.";
 		const expected =	new TypeError(error);
 
@@ -61,7 +61,7 @@ describe("These are test to the Pipedo class.", () => {
 	});
 
 	it("Test-8: returns false when pass only 2 values.", () => {
-		const current =		new Pipedo("100", "5");
+		const current =		new Hexaedro("100", "5");
 		const error =		"Please, provide a correct x, z or y value.";
 		const expected =	new TypeError(error);
 
