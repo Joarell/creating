f88cad74c73b980a9a2e4c768665746644791753
Joarell <josephjoarell@gmail.com>
2023-07-25 16:54:43 -0300

    build (test): adds the first steps to the crate test

---
+++ tests/Crate.test.mjs
@@ -0,0 +1,10 @@
+
+import Crate from '../www/app/core2/Crate.class.mjs';
+import { describe, it} from 'node:test';
+import assert from 'node:assert';
+
+
+describe("", () => {
+	it("", () => {
+	});
+});
