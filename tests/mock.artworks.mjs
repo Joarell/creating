1d6ac851078af55970733b164af00ace8170ded5
Joarell <josephjoarell@gmail.com>
2023-07-26 19:58:08 -0300

    chore (upaate): adds new mock functions to Arranger tests

--- tests/mock.artworks.mjs
+++ tests/mock.artworks.mjs
@@ -1,6 +1,7 @@
 
 import ArtWork from '../www/app/front-modules/Art.class.def.mjs';
 
+
 const list = [
 	['88800', 70, 70, 70],
 	['230202', 70, 70, 70],
@@ -38,7 +39,14 @@ const list = [
 	['11111', 60, 5, 60],
 	['22222', 60, 5, 60],
 	['33333', 60, 5, 60],
+	['LJ-10298', 220, 10, 300],
+	['LJ-298', 300, 10, 300],
 ]
+
+
+export function artList() {
+	return (list);
+};
 
 
 export function artWorksList() {
@@ -49,16 +57,95 @@ export function artWorksList() {
 	return(works);
 };
 
+export function largestWorks () {
+	const list =			noCanvasOut();
+	const { sorted } =		list;
+	const largestCanvas =	sorted.filter(work => {
+		const MAXHEIGHT = 220;
+		if (work[1] > MAXHEIGHT || work[3] > MAXHEIGHT)
+			return (work);
+		return ;
+	});
+
+	largestCanvas.map(canvas => {
+		list.sorted.splice(list.sorted.indexOf(canvas), 1);
+	});
+	list.largest = largestCanvas;
+	return(list);
+}
+
+
+export function noCanvasOut () {
+	const list =					lessSameSize();
+	const { sorted, sameSize } =	list;
+	const MAXDEPTH =				10;
+	const noCanvas =				sorted.filter(work => work[2] > MAXDEPTH);
+
+	sameSize.filter(work => {
+		if(work[2] > MAXDEPTH)
+			noCanvas.push(work);
+	});
+	noCanvas.map(art => {
+		const index = list.sorted.indexOf(art);
+		list.sorted.splice(index, 1);
+	});
+
+	list.noCanvas = noCanvas;
+	return (list);
+};
+
 
 export function lessSameSize() {
-	const works =		artWorksList();
-	const cubedList =	works.map(work => {
-		const cub = work.arr();
-		cub.push(work.cubed())
-		return(cub);
-	})
+	const sortedWorks =	quickSortResult();
+	const { sorted } =	sortedWorks;
+	const trialOne =	firstTrialWorks.call(sorted);
+	const trialTwo =	secondTrialWorks.call(trialOne);
+
+	trialTwo.map(art => {
+		sorted.splice(sorted.indexOf(art), 1);
+	});
+
+	return({sorted: sorted, sameSize: trialTwo});
+};
+
+
+function firstTrialWorks() {
+	const getter =	[];
+	const checker = (a, b) => a[4] === b[4] && a[0] !== b[0];
+	this.map(work => {
+		let i =	0;
+
+		for(i in this)
+			if (!getter.includes(this[i]) && checker(this[i], work))
+				getter.push(this[i])
+	});
 	
-	return ({ cubedList });
+	return(getter);
+};
+
+
+function secondTrialWorks() {
+	const sameSizeList = [];
+	this.map(work => {
+		let getter =	[];
+		const x =		work[1];
+		const y =		work[3];
+		const cub =		work[4];
+		let i =		0;
+		const check = (art) => art[1] === x && art[3] === y && art[4] === cub;
+
+		for(i in this) {
+			if (check(this[i]) && !sameSizeList.includes(this[i]))
+				getter.push(this[i]);	
+		};
+		if (getter.length >= 4)
+			getter.map(element => {
+				sameSizeList.push(element);
+			});
+		getter = null;
+	});
+
+	return(sameSizeList);
 };
 
 
@@ -99,9 +186,4 @@ export function artWorksCubed() {
 	
 	gc.add(works);
 	return (cubedList);
-};
-
-
-export function artList() {
-	return (list);
 };
