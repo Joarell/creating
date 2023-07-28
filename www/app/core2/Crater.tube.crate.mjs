6bc097ba15e7343a1f97da60ca854b9976989ab6
Joarell <josephjoarell@gmail.com>
2023-07-28 01:15:30 -0300

    build (calls): adds the new class already tested

---
+++ www/app/core2/Crater.tube.crate.mjs
@@ -0,0 +1,119 @@
+
+export default class CraterTube {
+	#tubes;
+
+	constructor(peces) {
+		if(!peces) 
+			return({ tube: false });
+
+		this.#tubes = peces;
+		return(this.#crateMaker());
+	};
+
+	#crateMaker() {
+		this.#possibleCrates();
+		return (this.#tubes);
+	};
+
+	#sizeComposer(){
+		let X = this[0][1];
+		let Z = this[0][2];
+		let Y = this[0][3];
+		
+		this.map(tube => {
+			X = tube[1] ?? X;
+			Z = tube[2] ?? Z;
+			Y = tube[3] ?? Y;
+		});
+
+		return([X, Z, Y]);
+	}
+
+	#setSizes(pad, highPad) {
+		const X = this[0] + pad;
+		const Z = this[1] + pad;
+		const Y = this[2] + highPad;
+
+		return([X, Z, Y]);
+	};
+
+	#oneTubeCrate() {
+		const DEFAULTPAD =	23;
+		const HEIGHTPAD =	25;
+		const X =			this[0][1] + DEFAULTPAD;
+		const Z =			this[0][2] + DEFAULTPAD;
+		const Y =			this[0][3] + HEIGHTPAD;
+
+		return ([X, Z, Y]);
+	};
+
+	#tubeCrate(works, content) {
+		const DEFAULTPAD =	15;
+		const HEIGHTPAD =	25 * content;
+		const baseSize =	this.#sizeComposer.call(works);
+
+		return (this.#setSizes.call(baseSize, DEFAULTPAD, HEIGHTPAD));
+	};
+
+	#interfaceCrates(opt, list) {
+		switch(opt) {
+			case 1:
+				return(this.#oneTubeCrate.call(list));
+			case 2:
+				return(this.#tubeCrate(list, 2));
+			case 3:
+				return(this.#tubeCrate(list, 3));
+			case 4:
+				return(this.#tubeCrate(list, 4));
+		};
+	};
+
+	#hugeTubes(tubes) {
+		const result =		[];
+		const MAXCONTENT =	3;
+		let getter;
+
+		while(tubes.length >= MAXCONTENT) {
+			getter = tubes.splice(0, MAXCONTENT);
+			result.push(this.#interfaceCrates(getter.length, getter));
+			result.push({ works: getter });
+		};
+		return (result);
+	};
+
+	#checkHugeTubes() {
+		const DIAMETER =	40;
+		const getter =		this.#tubes.filter(tube => {
+			if (tube[2] > DIAMETER)
+				return (tube);
+			return ;
+		});
+		getter.map(roll => {
+			this.#tubes.splice(this.#tubes.indexOf(roll), 1);
+		});
+		return (getter);
+	};
+
+	#possibleCrates() {
+		let reduce =		[];
+		let crates =		[];
+		const MAXCONTENT =	3;
+		const biggest =		this.#checkHugeTubes();
+
+		if (biggest.length > 0 || biggest.length > MAXCONTENT)
+			crates.push(this.#hugeTubes(biggest));
+
+		while(this.#tubes.length >= MAXCONTENT) {
+			reduce = this.#tubes.splice(0, MAXCONTENT);
+			crates.push(this.#interfaceCrates(reduce.length, reduce));
+			crates.push({ works: reduce });
+		};
+		if (this.#tubes.length >= 1) {
+			crates.push(this.#interfaceCrates(this.#tubes.length, this.#tubes));
+			crates.push({ works: this.#tubes });
+		};
+		return(this.#tubes = { crates: crates });
+	};
+};
+
+
