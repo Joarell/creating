495044f5de70fa0da4809248da96fbd34c6bb52c
Joarell <josephjoarell@gmail.com>
2023-07-28 01:22:56 -0300

    feat (update): adds new functions to to tube tests

--- tests/mock.artworks.mjs
+++ tests/mock.artworks.mjs
@@ -3,6 +3,9 @@ import ArtWork from '../www/app/front-modules/Art.class.def.mjs';
 
 
 const list = [
+	['909', 100, 20, 20],
+	['8383', 180, 30, 30],
+	['3231', 180, 30, 30],
 	['88800', 70, 70, 70],
 	['230202', 70, 70, 70],
 	['1298', 200, 5, 100],
@@ -43,6 +46,252 @@ const list = [
 	['LJ-298', 300, 10, 300],
 ]
 
+export const caseTube1 = [
+	['22222', 260, 20, 20],
+];
+
+const tube1 = [
+	['22222', 260, 20, 20],
+];
+
+export const caseTube2 = [
+	['98888', 100, 30, 30],
+	['22222', 260, 20, 20],
+];
+
+const tube2 = [
+	['98888', 100, 30, 30],
+	['22222', 260, 20, 20],
+];
+
+export const caseTube3 = [
+	['98888', 100, 30, 30],
+	['22222', 260, 20, 20],
+	['33332', 260, 20, 20],
+];
+
+const tube3 = [
+	['98888', 100, 30, 30],
+	['22222', 260, 20, 20],
+	['33332', 260, 20, 20],
+];
+
+export const caseTube4 = [
+	['98888', 100, 30, 30],
+	['11111', 260, 20, 20],
+	['22222', 260, 20, 20],
+	['33333', 260, 20, 20],
+];
+
+const tube4 = [
+	['98888', 100, 30, 30],
+	['11111', 260, 20, 20],
+	['22222', 260, 20, 20],
+	['33333', 260, 20, 20],
+];
+
+export const caseTube5 = [
+	['98888', 100, 30, 30],
+	['11111', 260, 20, 20],
+	['22222', 260, 20, 20],
+	['33330', 260, 20, 20],
+	['33331', 90, 20, 20],
+	['33328', 120, 40, 40],
+	['33319', 60, 20, 20],
+	['33319', 60, 20, 20],
+	['33269', 190, 35, 35],
+];
+
+const tube5 = [
+	['98888', 100, 30, 30],
+	['11111', 260, 20, 20],
+	['22222', 260, 20, 20],
+	['33330', 260, 20, 20],
+	['33331', 90, 20, 20],
+	['33328', 120, 40, 40],
+	['33319', 60, 20, 20],
+	['33319', 60, 20, 20],
+	['33269', 190, 35, 35],
+];
+
+export const caseTube6 = [
+	['98888', 100, 60, 60],
+	['11111', 260, 60, 60],
+	['22222', 260, 60, 60],
+	['33330', 260, 60, 60],
+	['33331', 190, 50, 50],
+	['33328', 120, 50, 50],
+	['33319', 200, 50, 50],
+	['33319', 200, 50, 50],
+	['33269', 280, 60, 60],
+];
+
+const tube6 = [
+	['98888', 100, 60, 60],
+	['11111', 260, 60, 60],
+	['22222', 260, 60, 60],
+	['33330', 260, 60, 60],
+	['33331', 190, 50, 50],
+	['33328', 120, 50, 50],
+	['33319', 200, 50, 50],
+	['33319', 200, 50, 50],
+	['33269', 280, 60, 60],
+];
+
+// ╭────────────────────────────────────────────────────────────╮
+// │ Bellow you will find the mock functions to 'Crater' class. │
+// ╰────────────────────────────────────────────────────────────╯
+
+function sizeComposer() {
+	let X = this[0][1];
+	let Z = this[0][2];
+	let Y = this[0][3];
+	
+	this.map(tube => {
+		X = tube[1] ?? X;
+		Z = tube[2] ?? Z;
+		Y = tube[3] ?? Y;
+	});
+
+	return([X, Z, Y]);
+};
+
+
+function setSizes(pad, highPad){
+	const X = this[0] + pad;
+	const Z = this[1] + pad;
+	const Y = this[2] + highPad;
+
+	return([X, Z, Y]);
+};
+
+
+function oneTubeCrate() {
+	const DEFAULTPAD =	23;
+	const HEIGHTPAD =	25;
+	const X =			this[0][1] + DEFAULTPAD;
+	const Z =			this[0][2] + DEFAULTPAD;
+	const Y =			this[0][3] + HEIGHTPAD;
+
+	return ([X, Z, Y]);
+};
+
+function TubeCrate(content) {
+	const DEFAULTPAD =	15;
+	const HEIGHTPAD =	25 * content;
+	const baseSize =	sizeComposer.call(this);
+
+	return (setSizes.call(baseSize, DEFAULTPAD, HEIGHTPAD));
+};
+
+
+function interfaceTubeCrates(opt, tubes) {
+	switch(opt) {
+		case 1:
+			return(oneTubeCrate.call(tubes));
+		case 2:
+			return(TubeCrate.call(tubes, 2));
+		case 3:
+			return(TubeCrate.call(tubes, 3));
+		case 4:
+			return(TubeCrate.call(tubes, 4));
+	};
+};
+
+
+function hugeTubes(huges) {
+	const result =		[];
+	const MAXCONTENT =	3;
+	let getter;
+
+	while(huges.length >= MAXCONTENT) {
+		getter = huges.splice(0, MAXCONTENT);
+		result.push(interfaceTubeCrates(getter.length, getter));
+		result.push({ works: getter });
+	};
+	return (result);
+};
+
+
+function checkHugeTubes () {
+	const DIAMETER =	40;
+	const getter =		this.filter(tube => {
+		if (tube[2] > DIAMETER)
+			return (tube);
+		return ;
+	});
+	getter.map(roll => {
+		this.splice(this.indexOf(roll), 1);
+	});
+	return (getter);
+};
+
+
+function crateTubesOpt(tubes) {
+	let reduce =		[];
+	let crates =		[];
+	const MAXCONTENT =	3;
+	const biggest =		checkHugeTubes.call(tubes);
+
+	if (biggest.length > 0 || biggest.length > MAXCONTENT)
+		crates.push(hugeTubes(biggest));
+
+	while(tubes.length >= MAXCONTENT) {
+		reduce = tubes.splice(0, MAXCONTENT);
+		crates.push(interfaceTubeCrates(reduce.length, reduce));
+		crates.push({ works: reduce });
+	};
+	if(tubes.length >= 1) {
+		crates.push(interfaceTubeCrates(tubes.length, tubes));
+		crates.push({ works: tubes });
+	}
+	return ({ crates: crates });
+};
+
+
+export function provideTubeCrate(opt) {
+	let tubes;
+	let crate = [];
+
+	switch(opt) {
+		case 1:
+			tubes = tube1;
+			break ;
+		case 2:
+			tubes = tube2;
+			break ;
+		case 3:
+			tubes = tube3;
+			break ;
+		case 4:
+			tubes = tube4;
+			break ;
+		case 5:
+			tubes = tube5;
+			break ;
+		case 6:
+			tubes = tube6;
+			break ;
+	};
+
+	if(tubes.length > 4) {
+		return(crateTubesOpt(tubes));
+	}
+	else
+		crate = crateTubesOpt(tubes); 
+	return (crate);
+};
+
+
+
+
+
+
+
+
+// ╭──────────────────────────────────────────────────────────────╮
+// │ Bellow you will find all mock functions to 'Arranger' class. │
+// ╰──────────────────────────────────────────────────────────────╯
 
 export function artList() {
 	return (list);
@@ -56,6 +305,39 @@ export function artWorksList() {
 
 	return(works);
 };
+
+
+export function findTubesClassTest () {
+	const list =			largestWorks();
+	const { noCanvas } =	list;
+	const tubes =			noCanvas.filter(pece => {
+		if(pece[1] !== pece[2] && pece[2] === pece[3])
+			return(pece);
+		return
+	})
+
+	tubes.map(art => list.noCanvas.splice(list.noCanvas.indexOf(art), 1));
+	list.tubes = tubes
+
+	return ( list );
+};
+
+
+export function findTubes () {
+	const list =			largestWorks();
+	const { noCanvas } =	list;
+	const tubes =			noCanvas.filter(pece => {
+		if(pece[1] !== pece[2] && pece[2] === pece[3])
+			return(pece);
+		return
+	})
+
+	tubes.map(art => list.noCanvas.splice(list.noCanvas.indexOf(art), 1));
+	list.tubes = tubes
+
+	return ({ list });
+};
+
 
 export function largestWorks () {
 	const list =			noCanvasOut();
