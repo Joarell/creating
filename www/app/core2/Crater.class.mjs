cfbf80aa4fa5d25c2617a8ab45d09c2ae1584988
Joarell <josephjoarell@gmail.com>
2023-07-29 17:38:16 -0300

    feat (method): adds the new method to handle largest canvas

--- www/app/core2/Crater.class.mjs
+++ www/app/core2/Crater.class.mjs
@@ -1,26 +1,34 @@
+import CraterPythagoras from "./Crater.largest.canvas.mjs";
+import CraterTube from "./Crater.tube.crate.mjs";
 
 
 export default class Crater {
-	#peces;
+	#lists;
 
 	constructor (list) {
-		this.#peces = list;
+		this.#lists = list;
 		return(this.#startCratelist());
 	};
 
 	#startCratelist () {
-		const finished = { crates: this.#peces };
+		const finished = { crates: this.#lists };
 		this.#tubeCrate();
+		this.#LargestCanvas();
 
 		retun(finished);
 	};
 	
+	// TODO: apply cubAir() to each crate.
 	#tubeCrate() {
-		const tubeCrate = new CraterTube(this.#peces.list?.tubes);
-		this.#peces.cratess = { tubeCrates: tubeCrate };
+		const tubeCrate = new CraterTube(this.#lists.list?.tubes);
+		if(tubeCrate.tubes)
+			this.#lists.crates = { tubeCrates: tubeCrate };
 	};
 
 	#LargestCanvas() {
+		const largestcrates = new CraterPythagoras(this.#lists.list?.largest);
+		if(largestcrates.largest)
+			this.#lists.crates = { largestCrate: largestcrates };
 	};
 	
 	#sameSizeCrate() {
