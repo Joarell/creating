d2980f5da133337c75ed56a4fb7c30a0683350de
Joarell <josephjoarell@gmail.com>
2023-07-29 17:36:42 -0300

    build (test): adds new testes to CraterPythagoras class

--- tests/Crater.test.mjs
+++ tests/Crater.test.mjs
@@ -5,6 +5,7 @@ import assert from 'node:assert';
 import * as mock from './mock.artworks.mjs';
 import Arranger from '../www/app/core2/Arranger.class.mjs';
 import CraterTube from '../www/app/core2/Crater.tube.crate.mjs';
+import CraterPythagoras from '../www/app/core2/Crater.largest.canvas.mjs';
 
 
 describe("These are test to Crater class.", () => {
@@ -59,6 +60,53 @@ describe("These are test to Crater class.", () => {
 		const works =		mock.caseTube6;
 		const current =		new CraterTube(works);
 		const expected =	mock.provideTubeCrate(6);
+	
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-8: returns false object to no list passe to the class.", () => {
+		const current =		new CraterPythagoras();
+		const expected =	{ largest: false };
+	
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-9: returns the Pythagoras crate to the list passed.", () => {
+		const largest =		mock.canvas1;
+		const current =		new CraterPythagoras(largest);
+		const expected =	mock.provideLargestCanvas(1);
+	
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-10: returns the Pythagoras crate to the list passed.", () => {
+		const largest =		mock.canvas2;
+		const current =		new CraterPythagoras(largest);
+		const expected =	mock.provideLargestCanvas(2);
+	
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-11: returns the Pythagoras crate to the list passed.", () => {
+		const largest =		mock.canvas3;
+		const current =		new CraterPythagoras(largest);
+		const expected =	mock.provideLargestCanvas(3);
+	
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-12: returns the Pythagoras crate to the list passed.", () => {
+		const largest =		mock.canvas4;
+		const current =		new CraterPythagoras(largest);
+		const expected =	mock.provideLargestCanvas(4);
+	
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-13: returns the Pythagoras crate to the list passed.", () => {
+		const largest =		mock.canvas5;
+		const current =		new CraterPythagoras(largest);
+		const expected =	mock.provideLargestCanvas(5);
 	
 		console.log(current, 'and', expected);
 		assert.deepStrictEqual(current, expected);
