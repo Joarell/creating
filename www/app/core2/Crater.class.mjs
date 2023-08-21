a4bf66f70bc591d0f083b679cd607a8ffefe6090
Joarell <josephjoarell@gmail.com>
2023-08-21 00:21:48 -0300

    build (methods): adds last class methods - lastCheckArrangerSameSizeToStandard - cubAir - totalCub - whichAirport

--- www/app/core2/Crater.class.mjs
+++ www/app/core2/Crater.class.mjs
@@ -1,16 +1,21 @@
 
+
 import CraterPythagoras from "./Crater.largest.canvas.mjs";
+import CraterLastCheckReArranger from "./Crater.last.check.mjs";
 import CraterNotCanvas from "./Crater.no.canvas.mjs";
 import CraterSameSize from "./Crater.same.size.mjs";
 import CraterStandard from "./Crater.standard.crate.mjs";
 import CraterTube from "./Crater.tube.crate.mjs";
+import CubCalc from "./CubCalc.class.mjs";
 
 
 export default class Crater {
-	#lists;
+	#list;
+	#crates;
 
 	constructor (list) {
-		this.#lists = list;
+		this.#list =	list;
+		this.#crates =	[];
 		return(this.#startCrateList());
 	};
 
@@ -19,51 +24,110 @@ export default class Crater {
 		this.#LargestCanvas();
 		this.#sameSizeCrate();
 		this.#noCanvasCrate();
-		this.#conventionalCrate();
+		this.#standardCrates();
+		this.#lastCheckArrangerSameSizeToStandard();
+		this.#cubAir();
+		this.#totalCub();
+		this.#whichAirPort();
 
-		return({ crates: this.#lists });
+		return({ crates: this.#crates });
 	};
 	
-	// TODO: apply cubAir() to each crate.
 	#tubeCrate() {
-		const tubeCrate = new CraterTube(this.#lists.list?.tubes);
-		if(tubeCrate.tubes)
-			this.#lists.crates = { tubeCrates: tubeCrate };
+		const tubeCrate = new CraterTube(this.#list?.tubes);
+		this.#crates.push({tubeCrate : tubeCrate});
 	};
 
 	#LargestCanvas() {
-		const largestcrates = new CraterPythagoras(this.#lists.list?.largest);
-		if(largestcrates.largest)
-			this.#lists.crates = { largestCrate: largestcrates };
+		const largestcrates = new CraterPythagoras(this.#list?.largest);
+		this.#crates.push({largestCrate : largestcrates});
 	};
 	
 	#sameSizeCrate() {
-		const sameMeasure = new CraterSameSize(this.#lists.list?.sameSize);
-		if(sameMeasure.largest)
-			this.#lists.crates = { sameSizeCrate: sameMeasure };
+		const sameMeasure = new CraterSameSize(this.#list?.sameSize);
+		this.#crates.push({sameSizeCrate : sameMeasure});
 	};
 
 	#noCanvasCrate() {
-		const noCanvas = new CraterNotCanvas(this.#lists.list?.noCanvas);
-		if(noCanvas.largest)
-			this.#lists.crates = { noCanvasCrate: noCanvas };
+		const noCanvas = new CraterNotCanvas(this.#list?.noCanvas);
+		this.#crates.push({noCanvasCrate : noCanvas});
 	};
 
-	#conventionalCrate() {
-		const conventinal = new CraterStandard(this.#lists.list?.sorted);
-		if(conventinal.largest)
-			this.#lists.crates = { noCanvasCrate: conventinal };
+	#standardCrates() {
+		const standard = new CraterStandard(this.#list?.sorted);
+		this.#crates.push({standardCrate : standard});
 	};
 
-	#lastCheckArranger() {
+	#lastCheckArrangerSameSizeToStandard() {
+		new CraterLastCheckReArranger(this.#crates);
+		// console.log(this.#crates[4].standardCrate.crates);
 	};
 
 	#cubAir() {
+		const setCub = (sizes) => {
+			if (sizes === false)
+				return ;
+			if (sizes.length === 3) {
+				const X =			sizes[0];
+				const Z =			sizes[1];
+				const Y =			sizes[2];
+				const cubCrate =	new CubCalc(X, Z, Y).cubCalcAir;
+
+				sizes.push(cubCrate);
+			};
+		};
+
+		this.#crates[0].tubeCrate.crates.map(crate => setCub(crate));
+		this.#crates[1].largestCrate.crates.map(crate => setCub(crate));
+		this.#crates[2].sameSizeCrate.crates.map(crate => setCub(crate));
+		this.#crates[3].noCanvasCrate.crates.map(crate => setCub(crate));
+		this.#crates[4].standardCrate.crates.map(crate => setCub(crate));
 	};
 
-	#totalcub() {
+	#totalCub() {
+		let total =	[];
+		const setTotalCub =	(crate) => {
+			if (crate === false)
+				return ;
+			if (crate.length === 4) {
+				total.push(crate[3]);
+			};
+		};
+
+		this.#crates[0].tubeCrate.crates.map(crate => setTotalCub(crate));
+		this.#crates[1].largestCrate.crates.map(crate => setTotalCub(crate));
+		this.#crates[2].sameSizeCrate.crates.map(crate => setTotalCub(crate));
+		this.#crates[3].noCanvasCrate.crates.map(crate => setTotalCub(crate));
+		this.#crates[4].standardCrate.crates.map(crate => setTotalCub(crate));
+		
+		total = total.reduce((sum, val) => sum + val, 0);
+		this.#crates.airCubTotal = total;
 	};
 
 	#whichAirPort () {
+		let pax =		0;
+		let cargo =		0;
+		const MAXX =	300;
+		const MAXZ =	200;
+		const MAXY =	160;
+		const wichAirport =	(crate) => {
+			if (crate === false)
+				return ;
+			if (crate.length === 4) {
+				const X = crate[0];
+				const Z = crate[1];
+				const Y = crate[2];
+
+				!(X > MAXX && Z > MAXZ || Y > MAXY) ? pax++ : cargo++;
+			};
+		};
+
+		this.#crates[0].tubeCrate.crates.map(crate => wichAirport(crate));
+		this.#crates[1].largestCrate.crates.map(crate => wichAirport(crate));
+		this.#crates[2].sameSizeCrate.crates.map(crate => wichAirport(crate));
+		this.#crates[3].noCanvasCrate.crates.map(crate => wichAirport(crate));
+		this.#crates[4].standardCrate.crates.map(crate => wichAirport(crate));
+
+		this.#crates.wichAirport = [{ PAX : pax }, { CARGO : cargo }];
 	};
 };
