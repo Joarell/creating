22905d8730e577abf19f57917bead05408a280f3
Joarell <josephjoarell@gmail.com>
2023-09-06 02:23:22 -0300

    build (render): finishes the class tube render - already tested

--- www/app/plotter/Tube.Render.class.mjs
+++ www/app/plotter/Tube.Render.class.mjs
@@ -1,47 +1,43 @@
 
+
 import * as coord from './layer.coordinate.mjs';
 
 export default class TubeRender {
 	#pixSize;
 	#tubes;
-	#innerCrate;
+	#inCrate;
 
 	constructor ({ works }, layerSize, dim) {
-		this.#tubes =		works;
-		this.#pixSize =		layerSize;
-		this.#innerCrate =	[dim[0] - 13.5, dim[1], dim[2] + 40];
+		this.#pixSize =	layerSize;
+		this.#inCrate =	[dim[0] + 5, dim[1], dim[2] + 3];
+		this.#tubes =	Array.isArray(works[0]) ? works : [works];
 
 		return (this.#tubeRender());
 	};
 
 	#worksPositionLayer(pixPoints) {
-		const rect =	document.createElementNS("http://www.w3.org/2000/svg", "rect");
-		const PAD =		10;
+		const RECT =	document.createElementNS("http://www.w3.org/2000/svg", "rect");
+		const PAD =		7;
 
-		rect.setAttribute("x", pixPoints.pixelX + PAD);
-		rect.setAttribute("y", pixPoints.pixelY + PAD);
-		rect.setAttribute("width", this.sizeX);
-		rect.setAttribute("height", this.sizeY);
-		return(rect);
+		RECT.setAttribute("y", pixPoints.pixelY + PAD);
+		RECT.setAttribute("x", pixPoints.pixelX + PAD);
+		RECT.setAttribute("width", this.sizeX);
+		RECT.setAttribute("height", this.sizeY);
+		return(RECT);
 	}
-
-	#reduceSpace(art, layer) {
-		layer[2] = layer[2]  - art[3];
-		return(layer);
-	};
 
 	#drawPoint (layer, crateSize, axios) {
 		const X =			layer[0] / crateSize[0];
 		const Y =			layer[2] / crateSize[2];
 		const DRAWPIXEL =	0;
-		const POSX =		X === 1 ? DRAWPIXEL : X * axios.x;
-		const POSY =		Y === 1 ? DRAWPIXEL : Y * axios.y;
+		const POSX =		X === 1 ? DRAWPIXEL : +(X * axios.x).toFixed(3);
+		const POSY =		Y === 1 ? DRAWPIXEL : +(Y * axios.y).toFixed(3);
 
+		console.log(X, Y);
 		return ({ pixelX: POSX, pixelY: POSY });
 	};
 
-	#textOnCenter(work, layer, crate) {
-		console.log(layer, 'and', crate);
+	#textOnCenter(work, layer, crate, localY) {
 		const text =	document.createElementNS("http://www.w3.org/2000/svg", "text");
 		const PADX =	14;
 		const MIDDLEX = this.sizeX * 1.5;
@@ -51,29 +47,30 @@ export default class TubeRender {
 			text.setAttribute("x", MIDDLEX - PADX);
 		else
 			text.setAttribute("x", CENTERX - PADX);
-		text.setAttribute("y", this.sizeY / 2);
+		text.setAttribute("y", this.sizeY / 2 + localY.pixelY + PADX);
 		text.innerHTML = work[0];
 		return (text);
 	}
 
 	#tubeRender () {
-		const node =	document.createDocumentFragment();
-		let size =		[...this.#innerCrate];
+		const element =	document.createDocumentFragment();
+		let size =		[...this.#inCrate];
 		let sizeY;
 		let sizeX;
 		let pixWork;
 		let points;
 
 		this.#tubes.map(tube => {
-			sizeX =	coord.proportion(tube[1], this.#pixSize.x, size[0]);
-			sizeY =	coord.proportion(tube[3], this.#pixSize.y, size[1]);
+			sizeX =	coord.proportion(tube[1], this.#pixSize.x, this.#inCrate[0]);
+			sizeY =	coord.proportion(tube[3], this.#pixSize.y, this.#inCrate[2]);
 			pixWork = { sizeX, sizeY };
-			points = this.#drawPoint(size, this.#innerCrate, this.#pixSize);
-			node.appendChild(this.#worksPositionLayer.call(pixWork, points));
-			node.appendChild(this.#textOnCenter.call(pixWork, tube, size, this.#innerCrate));
-			this.#pixSize.y = this.#pixSize.y - sizeY;
-			this.#reduceSpace(tube, size);
+			size[2] = size[2] - tube[3];
+			points = this.#drawPoint(size, this.#inCrate, this.#pixSize);
+			element.appendChild(this.#worksPositionLayer.call(pixWork, points));
+			element.appendChild(
+				this.#textOnCenter.call(pixWork, tube, size, this.#inCrate, points)
+			);
 		});
-		return (node);
+		return (element);
 	};
 }
