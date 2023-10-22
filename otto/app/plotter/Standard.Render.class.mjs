c41951cacdb1a078bc2cf2f11456276f5a39a7cf
Joarell <josephjoarell@gmail.com>
2023-10-22 07:53:27 -0300

    refactor (updates): redesigns the code to correct rendering

--- otto/app/plotter/Standard.Render.class.mjs
+++ otto/app/plotter/Standard.Render.class.mjs
@@ -5,12 +5,16 @@ export default class StandarRender {
 	#canvas;
 	#pixelSize;
 	#inCrate;
+	#filled;
 
 	constructor({ works }, layerSize, dim, layer) {
-		this.#pixelSize = layerSize;
-		this.#inCrate = dim;
-		this.#canvas = Object.values(works[layer])[0];
+		this.#pixelSize =	layerSize;
+		this.#filled =		JSON.parse(JSON.stringify(layerSize));
+		this.#inCrate =		dim;
+		this.#canvas =		Object.values(works[layer])[0];
 
+		this.#filled.x2 =	this.#filled.x;
+		this.#filled.y2 =	this.#filled.y;
 		return (this.#standardRender());
 	};
 
@@ -53,94 +57,129 @@ export default class StandarRender {
 		return (TEXT);
 	};
 
-	#verifyPlaceWork(data, valX, valY) {
-		let x;
-		let y;
-		const SIZEX =		this.#pixelSize.x;
-		const SIZEY =		this.#pixelSize.y;
-		const { next, values, pos } = data;
-		const postPos1 =	next[0] === null ? values[0] + valX : next[0] + valX;
-		const postPos2 =	next[1] === null ? values[1] + valY : next[1] + valY;
+	#updateInnerCrate(x, y) {
+		const test1 = this.#filled.y > 0 && y <= this.#filled.y;
+		const test2 = this.#filled.x > 0 && x <= this.#filled.x;
+		let test3;
+		let test4;
 
-		if (next[0] === null && next[1] === null)
-			return (false);
-		if (next[0] === null) {
-			if (postPos2 < SIZEY) {
-				x = pos[0]
-				y = next[1];
-			}
-			else {
-				x = next[1] + valY <= SIZEY && pos[0] === 0 ? 0 : values[0];
-				y = next[1] + valY <= SIZEY ? next[1] : undefined;
-			};
-		}
-		else {
-			postPos1 <= SIZEX ? x = next[0] : x = values[0];
-			pos[1] === 0 && next[0] ? y = 0 : y = values[1];
-		};
-		return (x !== undefined && y !== undefined ? { x, y } : false);
+		test1 ? this.#filled.y -= y : 0;
+		test2 ? this.#filled.x -= x : 0;
+
+		test3 = this.#filled.y === 0 && this.#filled.x2 >= x;
+		test4 = this.#filled.x === 0 && this.#filled.y2 >= y;
+
+		test3 ? this.#filled.x2 -= x : 0;
+		test4 ? this.#filled.y2 -= y : 0;
+		console.log(this.#filled);
 	};
 
-	#lastWorkUpdateNextValues(coord, data, x, y) {
-		const PIXELX =	this.#pixelSize.x;
-		const PIXELY =	this.#pixelSize.y;
-		const checkX1 =	coord.x === PIXELX || data.next[0] === null;
-		const checkX2 = data.values[0] + x === PIXELX;
-		const checkY1 = coord.y === PIXELY || data.values[1] + y === PIXELY;
-		const checkY2 = data.pos[0] === x && data.pos[1] + y < PIXELY;
+	#lastWorkUpdateNextValues(place, data, x, y) {
+		Object.entries(data).map((info) => {
+			const { fillX, fillY, pos, next } =	info[1];
+			const propX =	+(x / info[1].values[0]).toFixed(2);
+			const propY =	+(y / info[1].values[1]).toFixed(2);
+			const updateX =	place.y === next[1] && fillX + propX <= 1;
+			const updateY =	place.x === next[0] && fillY + propY <= 1;
 
-		checkX1 || checkX2 ? data.next[0] = null : data.next[0] = x + data.next[0];
-		checkY1 ? data.next[1] = null : checkY2 ? data.next[1] = null : false;
+			updateX ? info[1].fillX = +(fillX + propX).toFixed(2) : 0;
+			updateY ? info[1].fillY = +(fillY + propY).toFixed(2) : 0;
+			info[1].fillX >= 1 ? info[1].next[1] = null : 0;
+			info[1].fillY >= 1 ? info[1].next[0] = null : 0;
+		});
+		this.#updateInnerCrate(x, y);
+		return(data);
 	};
 
-	#setNewWork(code, data, previous, coord, x, y) {
+	#setNewWork(code, data, prev, coord, x, y) {
 		let nextX;
 		let nextY;
+		let fillX;
+		let fillY;
 		const PIXELX = this.#pixelSize.x;
 		const PIXELY = this.#pixelSize.y;
+		const testX = coord.x + prev.next[0] === PIXELX || this.#filled.x === coord.x;
+		const testY = coord.y + prev.next[1] === PIXELY || this.#filled.y === coord.y;
 
-		nextX = coord.x + x === PIXELX ? null :
-			coord.x === null && x === previous.pos[0] ?
-				null : PIXELX - (PIXELX - (coord.x + x));
-		nextY = coord.y + y === PIXELY && coord.y !== 0 ? null :
-			coord.y === 0 ? PIXELY - (PIXELY - y) :
-				PIXELY - (PIXELY - (coord.y + y));
+		this.#lastWorkUpdateNextValues(coord, data, x, y);
+		nextX = testY && coord.y === prev.next[1] ? null : coord.x + x;
+		nextY = testX && coord.x === prev.next[0] ? null : coord.y + y;
+		fillX = nextX === null || nextY >= PIXELY ? 1 : 0;
+		fillY = nextY === null || nextX >= PIXELX ? 1 : 0;
 		data[code] = {
 			values: [x, y],
 			pos: [coord.x, coord.y],
 			next: [nextX, nextY],
+			fillX,
+			fillY,
 		};
-		this.#lastWorkUpdateNextValues(coord, previous, x , y);
 		return (data);
-	}
+	};
+
+	#verifyPlaceWork(data, valX, valY) {
+		let x;
+		let y;
+		const { next, pos, values, fillX, fillY } = data;
+		const lastX =	pos[0] === 0 ? next[0] : pos[0];
+		const lastY =	pos[1] === 0 ? next[1] : pos[1];
+
+		if (lastX + valX <= this.#pixelSize.x) {
+			x = next[0];
+			y = pos[1];
+		}
+		else if (lastY + valY <= this.#pixelSize.y) {
+			x = pos[0];
+			y = next[1];
+		};
+		if (x >= this.#pixelSize.x && valX <= values[0]){
+			x = pos[0];
+			y = this.#pixelSize.y - this.#filled.y2;
+			console.log('Y2', y);
+		}
+		else if (this.#filled.x === 0 && next[0] !== null) {
+			x = next[0];
+			y = this.#pixelSize.y - this.#filled.y2;
+		};
+		x === 0 && fillX > 0 || x === null ? x = undefined : 0;
+		y === 0 && fillY > 0 || y === null ? y = undefined : 0;
+		return (x !== undefined && y !== undefined ? { x, y } : false);
+	};
 
 	#layoutMapWorks(info, weight, height, code) {
-		let work;
+		const ART =	code.at(-1);
+		let found =	0;
 		let result;
-		const ART =		code;
-		const SIZEX =	weight.at(-1);
-		const SIZEY =	height.at(-1);
+		let ref;
 
-		for (work in info) {
-			result = this.#verifyPlaceWork(info[work], SIZEX, SIZEY);
-			if (result) {
-				this.#setNewWork(ART, info, info[work], result, SIZEX, SIZEY);
-				return (info);
+		for (ref of code) {
+			result = this.#verifyPlaceWork(info[ref], weight, height);
+			if (result && found === 0) {
+				this.#setNewWork(ART, info, info[ref], result, weight, height);
+				found++;
 			};
 		};
+		return (info);
 	};
 
 	#layoutArranger(map, weigth, height, code) {
 		let x;
 		let y;
+		let fillX;
+		let fillY;
 
-		if (weigth.length === 1) {
-			x = this.#pixelSize.x - weigth.at(-1) === 0 ? null : weigth.at(-1);
-			y = this.#pixelSize.y - height.at(-1) === 0 ? null : height.at(-1);
+		if (Object.entries(map).length === 0) {
+			x = this.#pixelSize.x - weigth === 0 ? null : weigth;
+			y = this.#pixelSize.y - height === 0 ? null : height;
+			fillX = this.#filled.x + weigth === this.#pixelSize.x ? 1 : 0;
+			fillY = this.#filled.y + height === this.#pixelSize.y ? 1 : 0;
+			this.#filled.x -= x;
+			this.#filled.y -= y;
 			map[code] = {
-				values: [weigth.at(-1), height.at(-1)],
+				values: [weigth, height],
 				pos: [0, 0],
 				next: [x, y],
+				fillX,
+				fillY
 			};
 		}
 		else
@@ -160,21 +199,23 @@ export default class StandarRender {
 	};
 
 	#standardRender() {
-		const X =		[];
-		const Y =		[];
+		let x;
+		let y;
 		const MAPWORK = {};
 		const ICON =	`<i class="nf nf-oct-sync"></i>`;
+		const CODES =	[];
 
 		this.#canvas.map(art => {
 			if (art.at(-1) === ICON) {
-				X.push(coord.proportion(art[3], this.#pixelSize.x, this.#inCrate[0]));
-				Y.push(coord.proportion(art[1], this.#pixelSize.y, this.#inCrate[2]));
+				x = coord.proportion(art[3], this.#pixelSize.x, this.#inCrate[0]);
+				y = coord.proportion(art[1], this.#pixelSize.y, this.#inCrate[2]);
 			}
 			else {
-				X.push(coord.proportion(art[1], this.#pixelSize.x, this.#inCrate[0]));
-				Y.push(coord.proportion(art[3], this.#pixelSize.y, this.#inCrate[2]));
+				x = coord.proportion(art[1], this.#pixelSize.x, this.#inCrate[0]);
+				y = coord.proportion(art[3], this.#pixelSize.y, this.#inCrate[2]);
 			};
-			this.#layoutArranger(MAPWORK, X, Y, art[0]);
+			CODES.push(art[0]);
+			this.#layoutArranger(MAPWORK, x, y, CODES);
 		}, 0);
 		console.log(MAPWORK);
 		return(this.#drawAndWrite(MAPWORK));
