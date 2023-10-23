c12023bb26a5e9c8bfbc5bf54656e7bd5f2b1425
Joarell <josephjoarell@gmail.com>
2023-10-23 15:38:50 -0300

    fix (upate): improves the render arrange over the possible layer

--- otto/app/plotter/Standard.Render.class.mjs
+++ otto/app/plotter/Standard.Render.class.mjs
@@ -8,44 +8,44 @@ export default class StandarRender {
 	#filled;
 
 	constructor({ works }, layerSize, dim, layer) {
-		this.#pixelSize =	layerSize;
-		this.#filled =		JSON.parse(JSON.stringify(layerSize));
-		this.#inCrate =		dim;
-		this.#canvas =		Object.values(works[layer])[0];
+		this.#pixelSize = layerSize;
+		this.#filled = JSON.parse(JSON.stringify(layerSize));
+		this.#inCrate = dim;
+		this.#canvas = Object.values(works[layer])[0];
 
-		this.#filled.x2 =	this.#filled.x;
-		this.#filled.y2 =	this.#filled.y;
+		this.#filled.x2 = this.#filled.x;
+		this.#filled.y2 = this.#filled.y;
 		return (this.#standardRender());
 	};
 
-	#worksPositionLayer({next, pos, values }) {
-		const RECT =		document.createElementNS("http://www.w3.org/2000/svg", "rect");
-		const INSET =		1;
-		const PAD =			20;
-		const X =			this.#pixelSize.x;
-		const Y =			this.#pixelSize.y;
-		const EXTPADY =		0;
+	#worksPositionLayer({ next, pos, values }) {
+		const RECT = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+		const INSET = 1;
+		const PAD = 20;
+		const X = this.#pixelSize.x;
+		const Y = this.#pixelSize.y;
+		const EXTPADY = 0;
 		// const EXTPADY =		next[1] ? Y - next[1] : 0; // BUG: rendering wrong in some cases.
 
 		RECT.setAttribute("x", pos[0] + INSET);
 		RECT.setAttribute("y", pos[1] + EXTPADY + INSET);
 
 		values[0] >= X || pos[0] + values[0] + INSET >= X ?
-		RECT.setAttribute("width", values[0] - PAD) :
+			RECT.setAttribute("width", values[0] - PAD) :
 			RECT.setAttribute("width", values[0]);
 		values[1] + EXTPADY >= Y || pos[1] + values[1] >= Y ?
-		RECT.setAttribute("height", values[1] - PAD) :
+			RECT.setAttribute("height", values[1] - PAD) :
 			RECT.setAttribute("height", values[1]);
 		return (RECT);
 	};
-	
+
 	#textOnCenter({ next, pos, values }, code) {
-		const TEXT =		document.createElementNS("http://www.w3.org/2000/svg", "text");
-		const MID =			0.5;
+		const TEXT = document.createElementNS("http://www.w3.org/2000/svg", "text");
+		const MID = 0.5;
 		// const Y =			this.#pixelSize.y;
-		const EXTPADY =		0;
+		const EXTPADY = 0;
 		// const EXTPADY =	next[1] ? Y - next[1] : 0; // BUG: rendering wrong in some cases.
-		const LETTERPIX =	7;
+		const LETTERPIX = 7;
 		let posX;
 		let posY;
 
@@ -65,21 +65,19 @@ export default class StandarRender {
 
 		test1 ? this.#filled.y -= y : 0;
 		test2 ? this.#filled.x -= x : 0;
-
-		test3 = this.#filled.y === y || this.#filled.y2 === y;
+		test3 = this.#filled.y === 0;
 		test4 = this.#filled.x === 0;
-
 		test3 ? this.#filled.x2 -= x : 0;
 		test4 ? this.#filled.y2 -= y : 0;
 	};
 
 	#lastWorkUpdateNextValues(place, data, x, y) {
 		Object.entries(data).map((info) => {
-			const { fillX, fillY, next } =	info[1];
-			const propX =	+(x / info[1].values[0]).toFixed(2);
-			const propY =	+(y / info[1].values[1]).toFixed(2);
-			const updateX =	place.y === next[1] && fillX + propX <= 1;
-			const updateY =	place.x === next[0] && fillY + propY <= 1;
+			const { fillX, fillY, next } = info[1];
+			const propX = +(x / info[1].values[0]).toFixed(2);
+			const propY = +(y / info[1].values[1]).toFixed(2);
+			const updateX = place.y === next[1] && fillX + propX <= 1;
+			const updateY = place.x === next[0] && fillY + propY <= 1;
 
 			updateX ? info[1].fillX = +(fillX + propX).toFixed(2) : 0;
 			updateY ? info[1].fillY = +(fillY + propY).toFixed(2) : 0;
@@ -87,7 +85,7 @@ export default class StandarRender {
 			info[1].fillY >= 1 ? info[1].next[0] = null : 0;
 		});
 		this.#updateInnerCrate(x, y);
-		return(data);
+		return (data);
 	};
 
 	#setNewWork(code, data, prev, coord, x, y) {
@@ -119,40 +117,41 @@ export default class StandarRender {
 		let x;
 		let y;
 		const { next, pos, values, fillX, fillY } = data;
-		const lastX =	pos[0] === 0 ? next[0] : pos[0];
-		const lastY =	pos[1] === 0 ? next[1] : pos[1];
-		const testY =	fillY + valY / values[1] <= 1;
+		const lastX = pos[0] === 0 ? next[0] : pos[0];
+		const lastY = pos[1] === 0 ? next[1] : pos[1];
+		const testX = fillX + valX / values[0] <= 1;
+		const testY = fillY + valY / values[1] <= 1;
 
 		if (lastX + valX <= this.#pixelSize.x) {
 			x = next[0] + valX <= this.#pixelSize.x ? next[0] : undefined;
 			y = pos[1] + valY <= this.#pixelSize.y ? pos[1] : undefined;
 		}
 		else if (lastY + valY <= this.#pixelSize.y) {
-			x = pos[0] + valX <= this.#pixelSize.x  ? pos[0] : undefined;
+			x = pos[0] + valX <= this.#pixelSize.x ? pos[0] : undefined;
 			y = next[1] + valY <= this.#pixelSize.y ? next[1] : undefined;
 		};
-
 		x === 0 && fillX > 0 || x === null ? x = undefined : 0;
-		x === undefined && this.#filled.y >= valY ? x = pos[0] : 0;
+		!x && this.#filled.y >= valY ? x = pos[0] : 0;
+		!x && !testX && testY ? x = fillX * values[0] + pos[0] : 0;
 
 		y === 0 && !testY ? y = undefined : 0;
 		y === 0 && testY ? y = fillY * values[1] + pos[1] : 0;
-		y === 0 && this.#filled.x === 0 ? y = next[1]: 0;
+		y === 0 && this.#filled.x === 0 ? y = next[1] : 0;
 		return (x !== undefined && y !== undefined ? { x, y } : false);
 	};
 
 	#layoutMapWorks(info, weight, height, code) {
-		const ART =	code.at(-1);
-		let len =	code.length;
+		const ART = code.at(-1);
+		let len = code.length;
 		let result;
 		let ref;
 
 		for (ref of code) {
-			if (len-- > 1 ) {
+			if (len-- > 1) {
 				result = this.#verifyPlaceWork(info[ref], weight, height);
 				if (result) {
 					this.#setNewWork(ART, info, info[ref], result, weight, height);
-					break ;
+					break;
 				};
 			};
 		};
@@ -188,21 +187,21 @@ export default class StandarRender {
 		const element = document.createDocumentFragment();
 		let work;
 
-		for(work in table) {
+		for (work in table) {
 			element.appendChild(this.#worksPositionLayer(table[work]));
 			element.appendChild(this.#textOnCenter(table[work], work));
 		};
-		return(element);
+		return (element);
 	};
 
 	#standardRender() {
 		let x;
 		let y;
 		const MAPWORK = {};
-		const ICON =	`<i class="nf nf-oct-sync"></i>`;
-		const CODES =	[];
+		const ICON = `<i class="nf nf-oct-sync"></i>`;
+		const CODES = [];
 
-		this.#canvas.map( async art => {
+		this.#canvas.map(async art => {
 			if (art.at(-1) === ICON) {
 				x = coord.proportion(art[3], this.#pixelSize.x, this.#inCrate[0]);
 				y = coord.proportion(art[1], this.#pixelSize.y, this.#inCrate[2]);
@@ -214,6 +213,6 @@ export default class StandarRender {
 			CODES.push(art[0]);
 			await this.#layoutArranger(MAPWORK, x, y, CODES);
 		}, 0);
-		return(this.#drawAndWrite(MAPWORK));
+		return (this.#drawAndWrite(MAPWORK));
 	};
 };
