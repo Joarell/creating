d32cfa36b6cc5195c8703845822b833056885576
Joarell <josephjoarell@gmail.com>
2024-02-09 00:32:58 -0300

    build (bundler): updates the bundler files

--- otto/rollup.config.mjs
+++ otto/rollup.config.mjs
@@ -1,7 +1,5 @@
 // import { rollup } from 'rollup';
 import terser from '@rollup/plugin-terser';
-import css from 'rollup-plugin-css-only';
-// import OMT from '@surma/rollup-plugin-off-main-thread';
 
 export default {
 	input: './app/main.mjs',
@@ -10,32 +8,24 @@ export default {
 		clearScreen: false
 	},
 	output: [
-		{
-			file: './app/bundle.mjs',
-			format: 'esm'
-		},
+		// {
+		// 	file: './app/bundle.mjs',
+		// 	format: 'esm'
+		// },
 		{
 			file: './app/main.min.mjs',
 			format: 'esm',
+			assetFileNames: './app/[name]-[hash][extname]',
+			preserverModules: true,
 			plugins: [terser({
 				format: {
 					quote_style: 3,
 				},
+				keep_classnames: true,
 				ecma: 2023
 			})],
-		}
-		// {
-		// 	input: 'css.js',
-		// 	output: {
-		// 		file: 'output.js',
-		// 		format: 'esm',
-		// 		assetFileNames: 'assets/[name]-[hash].css'
-		// 	},
-		// 	plugins: [css({
-		// 		output: 'index.css'
-		// 	})]
-		// }
+		},
 	],
-	dir: "app",
-	assetFileNames: "app/[name]-[hash][extname]"
+	dir: "./app/",
+	assetFileNames: "./app/[name]-[hash][extname]"
 };
