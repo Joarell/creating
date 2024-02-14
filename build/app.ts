e3f2a49d34299fe9586c31dfb02f719e1ddebd07
Joarell <josephjoarell@gmail.com>
2024-02-14 20:41:33 -0300

    fix (test): testing commit

--- build/app.ts
+++ build/app.ts
@@ -7,5 +7,5 @@ function printer(user: User): void {
 	console.log(`${user.name} and ${user.age}`);
 };
 
-const user = { name: "Jev", age: 33 };
+const user = { name: "Jorge", age: 33 };
 printer(user);
