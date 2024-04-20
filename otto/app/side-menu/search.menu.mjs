1eb9082c9280f6fd8d56391dd36eb879d5a7ae40
Joarell <josephjoarell@gmail.com>
2024-04-20 20:54:29 -0300

    feat (fetch): improves the fetch front-end behavior

--- otto/app/side-menu/search.menu.mjs
+++ otto/app/side-menu/search.menu.mjs
@@ -1,3 +1,19 @@
+
+import * as status from '../front-modules/functions.front.end.mjs';
+
+
+function closeDialog() {
+	const closeDialog =	document.querySelector('.side-menu').childNodes;
+
+	closeDialog.length === 6 ? sessionStorage.setItem('CLOSED', 'NOW') : 0;
+	setTimeout(() => {
+		status.displayCub();
+		status.displayAirCub();
+		status.countWorks();
+	}, 200);
+};
+
+
 // NOTE: reset the code changed by the data base in order to render on status panel.
 /**
  * @param {Crater} list The Crater object fetched from the DB.
@@ -18,12 +34,12 @@ function resetList(list) {
  * @param {String} doc The reference/document with artwork list.
 */
 export async function checkBrowserDB(doc) {
-	const workerDB = new Worker(
+	const workerDB =	new Worker(
 		// new URL("../panels/worker.IDB.crates.mjs", import.meta.url),
 		new URL("./panels/worker.IDB.crates.mjs", import.meta.url),
 		{ type: "module" },
 	);
-	const checkIDB = await new Promise((resolve, reject) => {
+	const checkIDB =	await new Promise((resolve, reject) => {
 		workerDB.postMessage(doc);
 		workerDB.onmessage = (result) => {
 			result !== undefined ? resolve(result.data) : reject(undefined);
@@ -33,6 +49,7 @@ export async function checkBrowserDB(doc) {
 	if (checkIDB) {
 		document.getElementById("input_estimate").value = doc;
 		sessionStorage.setItem("FETCHED", JSON.stringify(checkIDB));
+		closeDialog();
 		return(setDBFetched([checkIDB]));
 	};
 	return fetchDB(doc);
@@ -58,11 +75,11 @@ function setDBFetched(result) {
 			document.getElementById("input_estimate").value = reference_id;
 			globalThis.sessionStorage.clear();
 			globalThis.sessionStorage.setItem("FETCHED", data);
+			closeDialog();
 		}
 		else throw new TypeError("Data not found!");
 	} catch (err) {
-		console.log(`ATTENTION: ${err}`);
-		alert(`Document not found! Please, try again.`);
+		//alert(`Document not found! Please, try again.`);
 	};
 };
 
@@ -84,7 +101,6 @@ async function fetchDB(doc) {
 			.then(setDBFetched)
 			.catch((err) => console.error(`ALERT ${err}`));
 		} catch (err) {
-			console.log(`ATTENTION: ${err}`);
 			alert(`Document not found on database! Please, try again.`);
 		}
 	}
