4840d819df2f15c7d03865329c2d03166b87ca2d
Joarell <josephjoarell@gmail.com>
2024-05-15 02:14:17 -0300

    fix : update log name

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-const log = require('debug')('api:back-server');
+const log = require('debug')('api:back_server');
 const express =	require('express');
 const router=	require('./router.js');
 const app =		express();
