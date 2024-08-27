f710643a387dbe35bf106fc47581edc644d80bd9
Joarell <josephjoarell@gmail.com>
2024-08-27 19:33:09 -0300

    fix (cache): set the cache properly

--- controllers/estimate.controller.js
+++ controllers/estimate.controller.js
@@ -94,8 +94,8 @@ const logoutUser = async (req, res) => {
 
 const setCacheLogin = async (user) => {
 	const checker = await cache.get(user.name);
-	const expTIME =	{EX: 28800};
-	return (checker ? 403 : cache.set(user.name, user.company, expTIME));
+	const expTIME =	28800;
+	return (checker ? 403 : cache.set(user.name, user.company, "EX", expTIME));
 };
 
 
